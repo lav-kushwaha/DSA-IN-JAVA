42f0a9f87d48afdb769b82fe74e193a4091943c2
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-03 23:14:54 +0530

    Intro to java

---
+++ src/_01/Introduction_To_Java/PrimeNumber.java
@@ -0,0 +1,24 @@
+package _01.Introduction_To_Java;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num = 18;
+        if (num == 1) {
+            System.out.println("Neither prime nor composite");
+        } else {
+            int count = 2;
+            boolean isPrime = true;
+            while (count * count <= num) {
+                if (num % count == 0) {
+                    isPrime = false;
+                    System.out.println("Number is not a prime number");
+                    break;
+                }
+                count++;
+            }
+            if (isPrime) {
+                System.out.println("Number is a prime number");
+            }
+        }
+    }
+}
