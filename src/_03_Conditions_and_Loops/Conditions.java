cd3cebe296467f10512fba1f5b2e60d1bcf2b149
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-06 21:38:54 +0530

    Conditions

---
+++ src/_03_Conditions_and_Loops/Conditions.java
@@ -0,0 +1,32 @@
+package _03_Conditions_and_Loops;
+public class Conditions {
+    public static void main(String[] args) {
+        //if statement
+//        int salary = 10000;
+//        if(salary>5000){
+//            salary = salary+5000;
+//        }
+//        System.out.println(salary);
+
+//        //if else statement
+//        int salary = 10000;
+//        if(salary>15000){
+//            salary = salary + 5000;
+//        }else {
+//            salary = salary - 5000;
+//        }
+//        System.out.println(salary);
+
+        //else if ladder.
+//        int salary = 1000;
+//        if(salary>15000){
+//            salary = salary + 5000;
+//        }else if(salary<5000){
+//            salary = salary - 5000;
+//        }else {
+//            System.out.println("Your salary is not valid");
+//        }
+//        System.out.println(salary);
+
+    }
+}
