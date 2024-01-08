7d01db4185578e03ea1e285d002352b5155098db
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-08 23:52:48 +0530

    Old style switch statements

--- src/_04_Switch_Statements/Enhance_Switch_Statements.java
+++ src/_04_Switch_Statements/Enhance_Switch_Statements.java
@@ -4,13 +4,40 @@ import java.util.Scanner;
 
 public class Enhance_Switch_Statements {
     public static void main(String[] args) {
+
+        //Enhance switch statements
         Scanner sc = new Scanner(System.in);
         int day = sc.nextInt();
 
-        switch(day){
-            case 1,2,3,4,5 -> System.out.println("Weekdays");
-            case 6,7 -> System.out.println("Weekend");
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekdays");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekend");
+                break;
         }
+
+
+        //old style switch statements.
+//        switch (day) {
+//            case 1:
+//            case 2:
+//            case 3:
+//            case 4:
+//            case 5:
+//                System.out.println("Weekdays");
+//                break;
+//            case 6:
+//            case 7:
+//                System.out.println("Weekend");
+//                break;
+//        }
 
 
     }
