d0f64eb5462e8cb257410165be82987d3b555f8e
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-11 23:48:21 +0530

    Arrays And ArrayList

---
+++ src/_06_Introduction_To_Arrays_And_ArrayList/Arrayz.java
@@ -0,0 +1,14 @@
+package _06_Introduction_To_Arrays_And_ArrayList;
+
+import java.util.Arrays;
+
+public class Arrayz {
+    public static void main(String[] args) {
+        String [] arr1 = new String[5];//[null, null, null, null, null]
+        int [] arr = new int[5];//[0, 0, 0, 0, 0]
+//        arr[0] = 1;
+//        int []  arr = {1,2,3,4,5};
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
