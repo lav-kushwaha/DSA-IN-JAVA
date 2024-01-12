8506f44754b45f1f602ebfbe22ab12e84c6c723a
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-12 19:20:07 +0530

    Arrays

--- src/_06_Introduction_To_Arrays_And_ArrayList/StringArrays.java
+++ src/_06_Introduction_To_Arrays_And_ArrayList/StringArrays.java
@@ -3,7 +3,6 @@ public class StringArrays {
     public static void main(String[] args) {
         // In Java, String objects are immutable. This means that once a String object is created, its content cannot be changed.
         // If you want to modify a String, a new String object is created with the desired modifications.
-
         String originalString = "Hello";
 
         // Concatenating a new string to the original one
