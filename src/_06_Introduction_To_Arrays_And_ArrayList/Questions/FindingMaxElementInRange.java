8d395b328e975939336418b97bc4cf197c6a3eed
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-13 22:21:15 +0530

    Arrays Coding Questions

---
+++ src/_06_Introduction_To_Arrays_And_ArrayList/Questions/FindingMaxElementInRange.java
@@ -0,0 +1,21 @@
+package _06_Introduction_To_Arrays_And_ArrayList.Questions;
+
+public class FindingMaxElementInRange {
+    public static void main(String[] args) {
+        int [] arr = {10,30,20,25,30,60};
+        int ans = MaxInRange(arr,2,4);
+        System.out.println(ans);
+    }
+    static int MaxInRange(int arr[], int start, int end){
+        //we can do this question using for-loop as well as.
+        int maxVal = arr[0];
+        while(start<=end){
+            if(maxVal<arr[start]){
+                maxVal=arr[start];
+                start++;
+            }
+        }
+        return maxVal;
+    }
+
+}
