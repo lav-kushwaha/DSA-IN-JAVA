7380646ce21f3c07e08d46c45dd69cd8b14f4b3e
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-13 21:49:50 +0530

    2D ArrayList

--- src/_06_Introduction_To_Arrays_And_ArrayList/ArrayList/ArrayListExample.java
+++ src/_06_Introduction_To_Arrays_And_ArrayList/ArrayList/ArrayListExample.java
@@ -1,11 +1,14 @@
 package _06_Introduction_To_Arrays_And_ArrayList.ArrayList;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListExample {
     public static void main(String[] args) {
+        //Syntax of ArrayList
         ArrayList<Integer> list = new ArrayList<Integer>(4);
 
+        //Methods of ArrayList
         //adding element in arrayList
         list.add(10);
         list.add(20);
@@ -17,8 +20,32 @@ public class ArrayListExample {
         //removing element from list
         list.remove(2);//index no.2: 30 will be removed.
 
+        //list.contains method gives true or false if number is present in the list the it will return true otherwise it will return false
+        System.out.println(list.contains(40));//output:true
+
+        //updating value using set : int index, int element
+        list.set(0,100); //it will replace number from 0th index and put 100
+
+        //if we have to get any items from list we can use get method
+        int ans = list.get(1);//it will return index 1 element which is 20
+        System.out.println(ans);
+
         //printing element
         System.out.println(list);
+
+
+        //using for-loop taking input in arrays.
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> lists = new ArrayList<>();
+
+        //Taking user input in arraylist.
+        for(int i=0;i<5;i++){
+            lists.add(sc.nextInt());
+        }
+        //printing arraylist using for loops.
+        for(int i=0;i<5;i++){
+            System.out.print(lists.get(i));//pass index here, list[index] will not work.
+        }
 
     }
 }
---
+++ src/_06_Introduction_To_Arrays_And_ArrayList/ArrayList/MultiDimensionalArrayList.java
@@ -0,0 +1,26 @@
+package _06_Introduction_To_Arrays_And_ArrayList.ArrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultiDimensionalArrayList {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        //Created ArrayList for adding elements into the list.
+        Scanner sc = new Scanner(System.in);
+
+        //Initialisation
+        for(int i=0;i<3;i++){
+            list.add(new ArrayList<>());
+        }
+
+        //add elements.
+        for (int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                list.get(i).add(sc.nextInt());
+            }
+        }
+
+        System.out.println(list);
+    }
+}
