fa8be5a0f94bee1e5dc19c9763b03d8abc815221
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-16 00:37:43 +0530

    OrderAgnosticBinarySearch

--- src/_07_Searching/BinarySearch/OrderAgnosticBinarySearch.java
+++ src/_07_Searching/BinarySearch/OrderAgnosticBinarySearch.java
@@ -2,39 +2,42 @@ package _07_Searching.BinarySearch;
 
 public class OrderAgnosticBinarySearch {
     public static void main(String[] args) {
-        int [] arr = {10,20,30,40,50,60,70};
-        int target = 50;
-        int ans = OrderAgnosticBinarySearch(arr,target);
+//        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int[] arr = {99, 80, 75, 22, 11, 10, 5, 2, -3};
+        int target = 22;
+        int ans = orderAgnosticBS(arr, target);
         System.out.println(ans);
     }
-    static int OrderAgnosticBinarySearch(int[] arr, int target){
+    static int orderAgnosticBS(int[] arr, int target) {
         int start = 0;
-        int end = arr.length-1;
-        // find whether the array is sorted in ascending or descending
-        boolean isAscending = arr[start]<arr[end];//ascending order.
+        int end = arr.length - 1;
 
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            if(arr[mid]==target) {
+        // find whether the array is sorted in ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
                 return mid;
             }
-            if(isAscending){
-                if(target<arr[mid]){
-                    end = mid -1;
-                }
-                else {
-                    start = start+1;
-                }
-            }else {
-                //This is for descending order.
-                // only sign change is : ">"
-                if(target>arr[mid]){
+            if (isAsc) {
+                if (target < arr[mid]) {
                     end = mid - 1;
-                }else {
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                //This is for descending order
+                //only sign change is : ">"
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
                     start = mid + 1;
                 }
             }
-
         }
         return -1;
     }
