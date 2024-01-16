e04b8c3d24ce84bdd28153e0df8946877349da75
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-16 22:48:10 +0530

    Binary Search

--- src/_07_Searching/BinarySearch/OrderAgnosticBinarySearch.java
+++ src/_07_Searching/BinarySearch/OrderAgnosticBinarySearch.java
@@ -2,7 +2,7 @@ package _07_Searching.BinarySearch;
 
 public class OrderAgnosticBinarySearch {
     public static void main(String[] args) {
-//        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        //int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
         int[] arr = {99, 80, 75, 22, 11, 10, 5, 2, -3};
         int target = 22;
         int ans = orderAgnosticBS(arr, target);
