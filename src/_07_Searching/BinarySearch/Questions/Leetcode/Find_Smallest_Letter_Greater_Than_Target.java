6cda63b99761c611115daec69da403fa0a2607fa
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-17 22:43:17 +0530

    Binary Search Questions

---
+++ src/_07_Searching/BinarySearch/Questions/Leetcode/Find_Smallest_Letter_Greater_Than_Target.java
@@ -0,0 +1,28 @@
+package _07_Searching.BinarySearch.Questions.Leetcode;
+
+//https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+public class Find_Smallest_Letter_Greater_Than_Target {
+    //this is like a ceiling just a little bit change.
+    static char nextGreatestLetter(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        // If left is within bounds, return letters[left], otherwise return letters[0]
+        return letters[left % letters.length];
+    }
+    public static void main(String[] args) {
+        char [] letters = {'c','f','j'};
+        char target = 'j';
+        char ans = nextGreatestLetter(letters,target);
+        System.out.println(ans);
+
+    }
+}
