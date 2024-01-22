01badbdf8a35de4dcc3eab278e4711edba9f806c
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-22 22:58:43 +0530

    Binary Search Questions

--- src/_07_Searching/BinarySearch/Questions/Leetcode/FindinMountainArray.java
+++ src/_07_Searching/BinarySearch/Questions/Leetcode/FindinMountainArray.java
@@ -1,6 +1,6 @@
 package _07_Searching.BinarySearch.Questions.Leetcode;
 //https://leetcode.com/problems/find-in-mountain-array/description/
-//1095. Find in Mountain Array
+//1095. Find in Mountain Array.
 public class FindinMountainArray {
     public static void main(String[] args) {
 
