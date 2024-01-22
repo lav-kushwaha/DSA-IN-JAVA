1129ab4d008b51b35d228f2b7201d49fd3b0ab91
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-22 17:56:07 +0530

    Binary Search Leetcode Questions

---
+++ src/Questions/BinaryArraySorting.java
@@ -0,0 +1,35 @@
+package Questions;
+//https://www.geeksforgeeks.org/problems/binary-array-sorting-1587115620/1?page=1&category=Arrays,Strings&difficulty=Basic&sortBy=submissions
+//Binary Array Sorting
+
+import java.util.Arrays;
+public class BinaryArraySorting {
+    public static void main(String[] args) {
+        int [] arr = {1,0,1,1,0};
+        int N = 5;
+        binSort(arr,N);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void binSort(int A[], int N)
+    {
+        int count = 0; // Count of 0's
+
+        // Traverse the array and count the number of 0's
+        for (int i = 0; i < N; i++) {
+            if (A[i] == 0) {
+                count++;
+            }
+        }
+
+        // Fill the first count elements with 0
+        for (int i = 0; i < count; i++) {
+            A[i] = 0;
+        }
+
+        // Fill the remaining elements with 1
+        for (int i = count; i < N; i++) {
+            A[i] = 1;
+        }
+
+    }
+}
---
+++ src/_07_Searching/BinarySearch/Questions/Leetcode/FindinMountainArray.java
@@ -0,0 +1,8 @@
+package _07_Searching.BinarySearch.Questions.Leetcode;
+//https://leetcode.com/problems/find-in-mountain-array/description/
+//1095. Find in Mountain Array
+public class FindinMountainArray {
+    public static void main(String[] args) {
+
+    }
+}
