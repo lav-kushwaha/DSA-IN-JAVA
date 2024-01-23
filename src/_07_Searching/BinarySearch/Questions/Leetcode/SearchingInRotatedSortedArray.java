22c19550d085c8825c5bbd9f00b65418b2acbd30
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-23 22:24:33 +0530

    Binary Search Questions

--- src/_07_Searching/BinarySearch/Questions/Leetcode/SearchingInRotatedSortedArray.java
+++ src/_07_Searching/BinarySearch/Questions/Leetcode/SearchingInRotatedSortedArray.java
@@ -2,7 +2,7 @@ package _07_Searching.BinarySearch.Questions.Leetcode;
 //https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 //33. Search in Rotated Sorted Array
 
-public class SearchinRotatedSortedArray {
+public class SearchingInRotatedSortedArray {
     public static void main(String[] args) {
         int [] arr ={4,5,6,7,0,1,2};
         int target = 0;
