9a37bb2d22d40943df201ffde4573dc187beb40b
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-28 23:25:14 +0530

    Binary Search In 2D Arrays

--- src/_07_Searching/BinarySearchIn2DArrays/BinarySearchIn2DArrays.java
+++ src/_07_Searching/BinarySearchIn2DArrays/BinarySearchIn2DArrays.java
@@ -9,15 +9,6 @@ public class BinarySearchIn2DArrays {
                     {33,34,38,50}
         };
 
-//        int sum = 0;
-//        for(int i=0;i<num.length;i++){
-//            for (int j=0;j<num[i].length;j++){
-//                 if(j==1||j==2 && i==j){
-//                     sum = sum + num[i][j];
-//                 }
-//            }
-//
-//        }
-//        System.out.println(sum);
+
     }
 }
