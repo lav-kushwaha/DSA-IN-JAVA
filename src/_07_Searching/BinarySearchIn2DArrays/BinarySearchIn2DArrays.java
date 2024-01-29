a77de6442f6677384bbdb2ffba5653dbc38480e0
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-29 23:46:29 +0530

    Binary Search In 2D Arrays

--- src/_07_Searching/BinarySearchIn2DArrays/BinarySearchIn2DArrays.java
+++ src/_07_Searching/BinarySearchIn2DArrays/BinarySearchIn2DArrays.java
@@ -1,5 +1,7 @@
 package _07_Searching.BinarySearchIn2DArrays;
 
+import java.util.Arrays;
+
 public class BinarySearchIn2DArrays {
     public static void main(String[] args) {
         int[][] num = {
@@ -8,5 +10,24 @@ public class BinarySearchIn2DArrays {
                     {28,29,37,49},
                     {33,34,38,50}
         };
+        int target = 29;
+        int[] ans = Searching(num , target);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] Searching(int[][] matrix, int target){
+        int row = 0;
+        int column = matrix.length-1;
+        while(row<matrix.length-1 && column>=0){
+            if(matrix[row][column] == target){
+                return new int[] {row,column};
+            }
+            if(matrix[row][column]<target){
+                 row++;
+            }
+            else{
+                column--;
+            }
+        }
+        return new int[]{-1,-1};
     }
 }
