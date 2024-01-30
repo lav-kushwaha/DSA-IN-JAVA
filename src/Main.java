37347671c076348922b1b4dd79b10e1378230c75
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-30 22:24:48 +0530

    Commit

--- src/Main.java
+++ src/Main.java
@@ -35,7 +35,7 @@ public class Main {
         }
     }
     // Helper function to reverse a sub-array in-place
-     static void reverseArray(int[] arr, int start, int end) {
+    static void reverseArray(int[] arr, int start, int end) {
         while (start < end) {
             // Swap elements at start and end indices
             int temp = arr[start];
