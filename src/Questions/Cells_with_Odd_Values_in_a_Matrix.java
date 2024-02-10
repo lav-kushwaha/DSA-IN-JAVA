3f35d0c4882ba84f80d0b7b9f765d6218d612737
Lav Kushwaha <lavkumar1224@gmail.com>
2024-02-11 00:33:14 +0530

    Leetcode Google coding Questions

---
+++ src/Questions/Cells_with_Odd_Values_in_a_Matrix.java
@@ -0,0 +1,39 @@
+package Questions;
+//https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/
+public class Cells_with_Odd_Values_in_a_Matrix {
+    public static void main(String[] args) {
+        int[][] indices = {
+                            {0,1},
+                            {1,1}
+                        };
+        int m = 2;
+        int n = 3;
+
+        int ans = oddCells(m, n, indices);
+        System.out.println(ans);
+    }
+    static int oddCells(int m, int n, int[][] indices){
+           int [] rows = new int[m];
+           int [] cols = new int[n];
+
+           for(int i=0;i<indices.length;i++){
+                 int rowIndex = indices[i][0];
+                 int colIndex = indices[i][1];
+
+                 rows[rowIndex]++;
+                 cols[colIndex]++;
+
+           }
+
+           int oddElementCount = 0;
+           for(int i=0;i<m;i++){
+               for(int j=0;j<n;j++){
+                   if(rows[i] + cols[j] % 2 !=0){
+                       oddElementCount++;
+                   }
+               }
+           }
+
+           return oddElementCount;
+    }
+}
