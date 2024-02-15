b1a95a98f11a215711b7e891f3c70f2116202c92
Lav Kushwaha <lavkumar1224@gmail.com>
2024-02-15 23:09:28 +0530

    Questions

--- src/Questions/BinaryArraySorting.java
+++ src/Questions/BinaryArraySorting.java
@@ -20,6 +20,7 @@ public class BinaryArraySorting {
                 count++;
             }
         }
+
         // Fill the first count elements with 0
         for (int i = 0; i < count; i++) {
             A[i] = 0;
