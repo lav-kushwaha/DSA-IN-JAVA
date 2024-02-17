7261c734ce94901677e3dbcf1c2ebcfb9b53372f
Lav Kushwaha <lavkumar1224@gmail.com>
2024-02-17 23:05:40 +0530

    Questions

--- src/Questions/MaxElement.java
+++ src/Questions/MaxElement.java
@@ -8,6 +8,6 @@ public class MaxElement {
                 max = arr[i];
             }
         }
-        System.out.println(max);
+        System.out.println("Maximum element is : "+max);
     }
 }
