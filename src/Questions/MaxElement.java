2ab1de848253ab684cb3160b20976059d1f861d1
Lav Kushwaha <lavkumar1224@gmail.com>
2024-02-17 23:04:33 +0530

    Questions

---
+++ src/Questions/MaxElement.java
@@ -0,0 +1,13 @@
+package Questions;
+public class MaxElement {
+    public static void main(String[] args) {
+        int [] arr = {10,23,200,35,56,80};
+        int max = 0;
+        for(int i=0;i<arr.length;i++){
+            if(max<arr[i]){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
