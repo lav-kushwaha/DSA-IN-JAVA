dbb71b11e1360ba13f4f0684683467f839967093
Lav Kushwaha <lavkumar1224@gmail.com>
2024-02-22 00:10:01 +0530

    Cyclic Sort

--- src/_08_Sorting/CyclicSort.java
+++ src/_08_Sorting/CyclicSort.java
@@ -1,7 +1,5 @@
 package _08_Sorting;
-
 import java.util.Arrays;
-
 public class CyclicSort {
     public static void main(String[] args) {
         int [] arr = {5,4,2,1,3};
@@ -9,14 +7,15 @@ public class CyclicSort {
         System.out.println(Arrays.toString(arr));
     }
     static void Cyclic(int [] arr){
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                int CorrectIndex = arr[i] - 1;
-                if(arr[CorrectIndex]!=arr[i]){
-                    int temp = arr[CorrectIndex];
-                    arr[CorrectIndex] = arr[i];
-                    arr[i] = temp;
-                }
+        int i = 0;
+        while(i<arr.length){
+            int CorrectIndex = arr[i]-1;
+            if(arr[CorrectIndex]!=arr[i]){
+                int temp = arr[CorrectIndex];
+                arr[CorrectIndex] = arr[i];
+                arr[i] = temp;
+            }else{
+                i++;
             }
         }
     }
