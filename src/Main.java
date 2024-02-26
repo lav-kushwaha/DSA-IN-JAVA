87a58db240d0e48c7877649053e36148b581f512
Lav Kushwaha <lavkumar1224@gmail.com>
2024-02-26 22:04:49 +0530

    Main

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,27 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // Example 1:
+        //Example 1 :
         int[] arr1 = {1, 2, 3, 4, 5};
         int N1 = 5;
         int K1 = 3;
         reverseInGroups(arr1, N1, K1);
         for (int value : arr1) {
-            System.out.print(value + " ");// // Output: 3 2 1 5 4
+            System.out.print(value + " ");// Output: 3 2 1 5 4
         }
-
         // Example 2:
-//        int[] arr2 = {5, 6, 8, 9};
-//        int N2 = 4;
-//        int K2 = 3;
-//        reverseInGroups(arr2, N2, K2);
-//        for (int value : arr1) {
-//            System.out.print(value + " ");// // Output: 8 6 5 9
-//        }
+        //int[] arr2 = {5, 6, 8, 9};
+        //int N2 = 4;
+        //int K2 = 3;
+        //reverseInGroups(arr2, N2, K2);
+        //for (int value : arr1) {
+        //System.out.print(value + " "); // Output: 8 6 5 9
+        // }
     }
-    
     public static void reverseInGroups(int[] arr, int N, int K) {
         int i = 0;
         // Iterate through the array in groups of size K
         while (i < N) {
-
             // Find the ending index of the current group.
             int endIndex = (i + K - 1 < N - 1) ? i + K - 1 : N - 1;
             // Reverse the current group.
