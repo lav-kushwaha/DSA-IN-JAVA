2e4420a3de2ba1c68e28abc8bf001eec0cb67572
Lav Kushwaha <lavkumar1224@gmail.com>
2024-03-14 02:20:26 +0530

    Maths For DSA

---
+++ src/_11_MathsForDSA/BitwiseAndNumberSystem/FindUniqueUsingXOR.java
@@ -0,0 +1,16 @@
+package _11_MathsForDSA.BitwiseAndNumberSystem;
+
+public class FindUniqueUsingXOR {
+    public static void main(String[] args) {
+        int [] arr = {2,3,4,1,2,1,3,6,4};
+        System.out.println(UniqueElement(arr));
+    }
+    static int UniqueElement(int[] arr){
+        int unique = 0;
+        for(int num : arr){
+            unique^=num;
+        }
+        return unique;
+    }
+
+}
