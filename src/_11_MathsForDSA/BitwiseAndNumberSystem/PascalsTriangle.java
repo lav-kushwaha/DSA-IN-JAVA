02aa35daa227f2b694e3e4aa2313eb2e4673e5e7
Lav Kushwaha <lavkumar1224@gmail.com>
2024-03-16 01:23:03 +0530

    Pascals Triangle

---
+++ src/_11_MathsForDSA/BitwiseAndNumberSystem/PascalsTriangle.java
@@ -0,0 +1,23 @@
+package _11_MathsForDSA.BitwiseAndNumberSystem;
+//https://www.geeksforgeeks.org/sum-of-all-elements-up-to-nth-row-in-a-pascals-triangle/
+//if you don't understand check it. gfg there is best explaination given.
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println("Sum of all elements:"
+                + calculateSum(n));
+    }
+    static long calculateSum(int n)
+    {
+        // Initialize sum with 0
+        long sum = 0;
+
+        // Loop to calculate power of 2
+        // upto n and add them
+        for (int row = 0; row < n; row++) {
+            sum = sum + (1 << row);
+        }
+
+        return sum;
+    }
+}
