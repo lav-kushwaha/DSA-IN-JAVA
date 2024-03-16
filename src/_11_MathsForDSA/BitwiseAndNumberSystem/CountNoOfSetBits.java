a6c9330eb5cc940018c0c39a838c77d5b8835c73
Lav Kushwaha <lavkumar1224@gmail.com>
2024-03-16 16:07:26 +0530

    Count No of Bits

---
+++ src/_11_MathsForDSA/BitwiseAndNumberSystem/CountNoOfSetBits.java
@@ -0,0 +1,15 @@
+package _11_MathsForDSA.BitwiseAndNumberSystem;
+
+public class CountNoOfSetBits {
+    public static void main(String[] args) {
+        int n = 6;
+        int count = 0;
+        while (n!=0){
+            if((n&1)==1){
+                count++;
+            }
+            n=n>>1;
+        }
+        System.out.println(count);
+    }
+}
