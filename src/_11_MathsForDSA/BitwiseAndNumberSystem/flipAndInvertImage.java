80df8180373e94ba1043e1658740a2cfaad855d4
Lav Kushwaha <lavkumar1224@gmail.com>
2024-03-17 00:04:58 +0530

    Flip and InvertImage

---
+++ src/_11_MathsForDSA/BitwiseAndNumberSystem/flipAndInvertImage.java
@@ -0,0 +1,38 @@
+package _11_MathsForDSA.BitwiseAndNumberSystem;
+import java.util.Arrays;
+//https://leetcode.com/problems/flipping-an-image/description/
+//832. Flipping an Image
+public class flipAndInvertImage {
+    public static void main(String[] args) {
+        int [][] image = {
+                        {1,1,0},
+                        {1,0,1},
+                        {0,0,0}
+                        };
+        int[][] output = flipAndInvertImages(image);
+        for(int[] ans : output ){
+           System.out.println(Arrays.toString(ans));
+        }
+    }
+    public static int[][] flipAndInvertImages(int[][] image) {
+
+        //First reverse each row
+        for(int i=0;i<image.length;i++){
+            for(int j=0;j<image[i].length/2;j++){
+                int temp = image[i][j];
+                image[i][j] = image[i][image[i].length-1-j];
+                image[i][image[i].length-1-j] = temp;
+            }
+        }
+
+        //XORing elements by 1 to invert the element.
+        for(int row=0;row<image.length;row++){
+            for(int col=0;col<image[row].length;col++){
+                image[row][col] = image[row][col] ^ 1;
+            }
+        }
+
+        return image;
+
+    }
+}
