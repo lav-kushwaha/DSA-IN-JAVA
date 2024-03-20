e6f25f1b803bd642cadc3e210be180e63866eeee
Lav Kushwaha <lavkumar1224@gmail.com>
2024-03-21 02:01:49 +0530

    Sum Of Digits Using Recursion

--- src/_14_Recursion_Questions/Q4_SumOfDigits.java
+++ src/_14_Recursion_Questions/Q4_SumOfDigits.java
@@ -1,2 +1,29 @@
-package _14_Recursion_Questions;public class Q4_SumOfEntireDigits {
+package _14_Recursion_Questions;
+
+public class Q4_SumOfDigits {
+    public static void main(String[] args) {
+        int n = 1342;
+//      int ans = DigitsSum(n);
+        int ans = DigitsSumUsingRecursion(n);
+        System.out.println(ans);
+    }
+
+    //simple way to sum of digits.
+//    static int DigitsSum(int n){
+//        int sum = 0;
+//        while(n!=0){
+//            int digit = n%10;
+//            sum+=digit;
+//            n/=10;
+//        }
+//        return sum;
+//    }
+
+    //using recursion for sum of digits.
+    static int DigitsSumUsingRecursion(int n){
+        if(n==0){
+            return 0;
+        }
+        return (n%10)+DigitsSumUsingRecursion(n/10);
+    }
 }
