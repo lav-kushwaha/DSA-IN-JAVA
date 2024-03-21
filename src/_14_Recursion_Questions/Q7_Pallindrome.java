1f5bcc4b00fd13311e7288984e1f15966054c1cd
Lav Kushwaha <lavkumar1224@gmail.com>
2024-03-21 21:47:36 +0530

    Pallindrome Using Recursion

---
+++ src/_14_Recursion_Questions/Q7_Pallindrome.java
@@ -0,0 +1,25 @@
+package _14_Recursion_Questions;
+
+public class Q7_Pallindrome {
+
+    static int ReverseNum(int n){
+        int digits = (int)Math.log10(n)+1;
+        return helper(n,digits);
+    }
+
+    static int helper(int n, int digits){
+        if(n%10==n){
+            return n;
+        }
+        int rem = n%10;
+        return rem*(int)(Math.pow(10,digits-1))+helper(n/10,digits-1);
+    }
+
+    static boolean Pallindrome(int n){
+        return n == ReverseNum(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Pallindrome(121));
+    }
+}
