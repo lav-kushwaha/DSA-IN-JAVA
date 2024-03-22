dafa6640d613d513d959e9e0cbf5ba0bbe5c51d3
Lav Kushwaha <lavkumar1224@gmail.com>
2024-03-23 02:07:38 +0530

    Find All Index of target Value using Recursion

--- src/_14_Recursion_Questions/Recursion_Array_Questions/Q2_LinearSearchUsingRecursion.java
+++ src/_14_Recursion_Questions/Recursion_Array_Questions/Q2_LinearSearchUsingRecursion.java
@@ -1,12 +1,17 @@
 package _14_Recursion_Questions.Recursion_Array_Questions;
 
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+
 public class Q2_LinearSearchUsingRecursion {
     public static void main(String[] args) {
-        int[] arr ={};
-        int target = 18;
+        int[] arr ={1,4,8,5,18,20,20,25};
+        int target = 20;
         System.out.println("First Index: " + findFirstIndex(arr, target, 0));
         System.out.println("Last Index: " + findLastIndex(arr, target, arr.length - 1));
         System.out.println("Target Found: " + search(arr, target, 0));
+        findAllIndex(arr,target,0);
+        System.out.println(list);
     }
 
     // Find the first occurrence of the target value index.
@@ -37,5 +42,17 @@ public class Q2_LinearSearchUsingRecursion {
             return false;
         }
         return arr[index] == target || search(arr, target, index + 1);
+    }
+
+    //Find the all index of target value.
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void findAllIndex(int[] arr,int target,int index){
+        if(index==arr.length){
+            return;
+        }
+        if(arr[index]==target){
+            list.add(index);
+        }
+        findAllIndex(arr,target,index+1);
     }
 }
