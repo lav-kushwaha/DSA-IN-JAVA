f3dad9093247a82e5689eadfca0ceba073a54e36
Lav Kushwaha <lavkumar1224@gmail.com>
2024-03-25 00:16:33 +0530

    Return an ArrayList

--- src/_14_Recursion_Questions/Recursion_Array_Questions/Q3_Return_an_ArrayList.java
+++ src/_14_Recursion_Questions/Recursion_Array_Questions/Q3_Return_an_ArrayList.java
@@ -7,15 +7,35 @@ public class Q3_Return_an_ArrayList {
         int target = 4;
         //Passing ArrayList as an arguments.
         //i.e, ArrayList<Integer> list = new ArrayList<>();
-        System.out.println(FindIndexes(arr, target,0, new ArrayList<>()));//[3,4]
+//        System.out.println(FindIndexes(arr, target,0, new ArrayList<>()));//[3,4]
+
+        System.out.println(FindAllIndexes(arr,target,0));
     }
-     static ArrayList<Integer> FindIndexes(int[] arr, int target,int index, ArrayList<Integer> list) {
+
+    //Method - 01 Passing arraylist as an argument, and returning arraylist.
+//     static ArrayList<Integer> FindIndexes(int[] arr, int target,int index, ArrayList<Integer> list) {
+//        if(index==arr.length){
+//            return list;
+//        }
+//        if(target == arr[index]){
+//            list.add(index);
+//        }
+//        return FindIndexes(arr, target,index+1,list);
+//     }
+
+    //Method - 02 without passing ArrayList as an argument, returning list.
+    static ArrayList<Integer> FindAllIndexes(int[] arr, int target,int index) {
+        //don't used this approach bcz list created again and again.
+        ArrayList<Integer> list = new ArrayList<>();
         if(index==arr.length){
             return list;
         }
         if(target == arr[index]){
             list.add(index);
         }
-        return FindIndexes(arr, target,index+1,list);
-     }
+       ArrayList<Integer> ansFromBelowCalls  = FindAllIndexes(arr, target,index+1);
+       list.addAll(ansFromBelowCalls);
+       return list;
+    }
+
 }
