367a12f5002739169817511819ad3174389f6d8b
Lav Kushwaha <lavkumar1224@gmail.com>
2024-03-25 00:25:50 +0530

    Return an ArrayList

--- src/_14_Recursion_Questions/Recursion_Array_Questions/Q3_Return_an_ArrayList.java
+++ src/_14_Recursion_Questions/Recursion_Array_Questions/Q3_Return_an_ArrayList.java
@@ -7,21 +7,21 @@ public class Q3_Return_an_ArrayList {
         int target = 4;
         //Passing ArrayList as an arguments.
         //i.e, ArrayList<Integer> list = new ArrayList<>();
-//        System.out.println(FindIndexes(arr, target,0, new ArrayList<>()));//[3,4]
+        System.out.println(FindIndexes(arr, target,0, new ArrayList<>()));//[3,4]
 
         System.out.println(FindAllIndexes(arr,target,0));
     }
 
     //Method - 01 Passing arraylist as an argument, and returning arraylist.
-//     static ArrayList<Integer> FindIndexes(int[] arr, int target,int index, ArrayList<Integer> list) {
-//        if(index==arr.length){
-//            return list;
-//        }
-//        if(target == arr[index]){
-//            list.add(index);
-//        }
-//        return FindIndexes(arr, target,index+1,list);
-//     }
+     static ArrayList<Integer> FindIndexes(int[] arr, int target,int index, ArrayList<Integer> list) {
+        if(index==arr.length){
+            return list;
+        }
+        if(target == arr[index]){
+            list.add(index);
+        }
+        return FindIndexes(arr, target,index+1,list);
+     }
 
     //Method - 02 without passing ArrayList as an argument, returning list.
     static ArrayList<Integer> FindAllIndexes(int[] arr, int target,int index) {
