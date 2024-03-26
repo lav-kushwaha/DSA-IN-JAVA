9109e94d5dc2b6a507c2b1c9be405b2287ce37b8
Lav Kushwaha <lavkumar1224@gmail.com>
2024-03-27 00:11:39 +0530

    Print Triangle

--- src/_14_Recursion_Questions/Recursion_Pattern_Questions/Q2_PrintTriangle.java
+++ src/_14_Recursion_Questions/Recursion_Pattern_Questions/Q2_PrintTriangle.java
@@ -2,6 +2,18 @@ package _14_Recursion_Questions.Recursion_Pattern_Questions;
 
 public class Q2_PrintTriangle {
     public static void main(String[] args) {
-
+        triangle(5,0);
+    }
+    static void triangle(int r,int c){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            triangle(r,c+1);
+            System.out.print("*");
+        }else{
+            triangle(r-1,0);
+            System.out.println(" ");
+        }
     }
 }
