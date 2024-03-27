2b5e381415950beb4704cd514fa9815df06e8d89
Lav Kushwaha <lavkumar1224@gmail.com>
2024-03-27 22:48:30 +0530

    Selection Sort Using Recursion

---
+++ src/_14_Recursion_Questions/Recursion_Pattern_Questions/Q4_SelectionSort_Using_Recursion.java
@@ -0,0 +1,28 @@
+package _14_Recursion_Questions.Recursion_Pattern_Questions;
+
+import java.util.Arrays;
+
+public class Q4_SelectionSort_Using_Recursion {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 3, 5};
+        selection(arr, arr.length, 0, 0);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void selection(int[] arr, int r, int c, int max) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            if (arr[c] > arr[max]) {
+                selection(arr, r, c+1, c);
+            } else {
+                selection(arr, r, c+1, max);
+            }
+        } else {
+            int temp = arr[max];
+            arr[max] = arr[r-1];
+            arr[r-1] = temp;
+            selection(arr, r-1, 0, 0);
+        }
+    }
+}
