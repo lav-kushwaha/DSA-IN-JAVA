691f2c701fa21a2184203351066a73c6b262ad3e
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-05 23:06:50 +0530

    Skip Character

--- src/_14_Recursion_Questions/Recursion_Subset_and_Subsequence_String_Questions/Q1_Skip_A_Character.java
+++ src/_14_Recursion_Questions/Recursion_Subset_and_Subsequence_String_Questions/Q1_Skip_A_Character.java
@@ -3,6 +3,8 @@ package _14_Recursion_Questions.Recursion_Subset_and_Subsequence_String_Question
 public class Q1_Skip_A_Character {
     public static void main(String[] args) {
         Skip("","baaccluvaa");
+        String ans = Skip("baaccluvaa");
+        System.out.println(ans);
     }
     static void Skip(String p,String up){
         if(up.isEmpty()){
@@ -17,6 +19,21 @@ public class Q1_Skip_A_Character {
         }
         else {
            Skip(p+ch,up.substring(1));
+        }
+    }
+
+    static String  Skip(String up){
+        if(up.isEmpty()){
+            return " ";
+        }
+
+        char ch = up.charAt(0);
+
+        if(ch == 'a'){
+            return Skip(up.substring(1));
+        }
+        else {
+            return ch+Skip(up.substring(1));
         }
     }
 }
