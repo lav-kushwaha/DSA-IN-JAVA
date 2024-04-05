2bdd678d6c0664bb5d20c1a6b3dc9f3ddc6768eb
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-06 03:28:30 +0530

    Substring

---
+++ src/_14_Recursion_Questions/Recursion_Subset_and_Subsequence_String_Questions/Q3_Substring.java
@@ -0,0 +1,21 @@
+package _14_Recursion_Questions.Recursion_Subset_and_Subsequence_String_Questions;
+
+public class Q3_Substring {
+    public static void main(String[] args) {
+        Substring(" ","abc");
+    }
+
+     static void Substring(String p,String up) {
+        if(up.isEmpty()){
+            System.out.print(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        // char ch take it.
+         Substring(p+ch,up.substring(1));
+         //char ch ignore it.
+         Substring(p,up.substring(1));
+
+     }
+}
