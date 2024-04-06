989d67fbcd81b3982c6bfeafe9757fe912b13f61
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-06 23:31:28 +0530

    Returning Substring as a ArrayList

---
+++ src/_14_Recursion_Questions/Recursion_Subset_and_Subsequence_String_Questions/Q4_Substring_ArrayList_Return.java
@@ -0,0 +1,26 @@
+package _14_Recursion_Questions.Recursion_Subset_and_Subsequence_String_Questions;
+
+import java.util.ArrayList;
+
+public class Q4_Substring_ArrayList_Return {
+    public static void main(String[] args) {
+        System.out.println(Substring("","abc"));
+    }
+
+    static ArrayList<String> Substring(String p,String up){
+        if (up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> left = Substring(p+ch,up.substring(1));
+        ArrayList<String> right = Substring(p,up.substring(1));
+
+        left.addAll(right);
+
+        return left;
+    }
+}
