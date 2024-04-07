8cf2dbdaf0d619db67de171771514b768faa9679
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-08 01:19:50 +0530

    Subsets II

--- src/_14_Recursion_Questions/Recursion_Subset_and_Subsequence_String_Questions/Q7_Subsets_II.java
+++ src/_14_Recursion_Questions/Recursion_Subset_and_Subsequence_String_Questions/Q7_Subsets_II.java
@@ -1,6 +1,7 @@
 package _14_Recursion_Questions.Recursion_Subset_and_Subsequence_String_Questions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Q7_Subsets_II {
@@ -9,20 +10,26 @@ public class Q7_Subsets_II {
         List<List<Integer>> ans = subset(arr);
         System.out.print(ans+" ");
     }
-
-    private static List<List<Integer>> subset(int[] arr) {
-        List<List<Integer>> outer = new ArrayList<>();// [ [] [1]] //empty list we added like this :- [[]]
-
-        outer.add(new ArrayList<>());//[[]]
-
-        for (int num: arr){        //1 2 3
-            int n = outer.size();  // 1
-            for (int i=0; i<n;i++){ // 0
-                List<Integer> internal = new ArrayList<>(outer.get(i)); //[1]
-                internal.add(num); //1
-                outer.add(internal); //[1] //indivisual list added to the main list using this line of code.
-            }
-        }
-        return outer;
+     static List<List<Integer>> subset(int[] arr) {
+         Arrays.sort(arr);
+         List<List<Integer>> outer = new ArrayList<>();// [[], [1], [2], [1, 2]] //empty list we added like this :- [[]]
+         outer.add(new ArrayList<>());//[[]]
+         int start = 0;
+         int end = 0; //3
+         for(int i=0;i<arr.length;i++)  {
+             start = 0;
+             //if current and previous element is same, s = e+1
+             if(i>0 && arr[i] == arr[i-1]){
+                 start = end + 1; // 4
+             }
+             end = outer.size()-1;//3
+             int n = outer.size();  //4
+             for (int j=start;j<n;j++){
+                 List<Integer> internal = new ArrayList<>(outer.get(j));
+                 internal.add(arr[i]);
+                 outer.add(internal);
+             }
+         }
+         return outer;
     }
 }
