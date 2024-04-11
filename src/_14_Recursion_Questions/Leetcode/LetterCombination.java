e003f9488f365dbb7173be477663cba44bc1c83a
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-12 00:00:18 +0530

    Letter Combinations

--- src/_14_Recursion_Questions/Leetcode/LetterCombination.java
+++ src/_14_Recursion_Questions/Leetcode/LetterCombination.java
@@ -5,8 +5,26 @@ import java.util.List;
 //By K.k
 public class LetterCombination {
     public static void main(String[] args) {
-        System.out.println();
+        pad("","12");
+        System.out.println(letterCombinations("","12"));
+        System.out.println(padCount("","12"));
     }
+
+    //print letter combinations.
+    static void pad(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            pad(p + ch, up.substring(1));
+        }
+
+    }
+
+    //return letter combintions as a list
     static List<String> letterCombinations(String p,String up){
         if(up.isEmpty()){
             List<String> list = new ArrayList<>();
@@ -14,7 +32,8 @@ public class LetterCombination {
             return list;
         }
 
-        int digits = up.charAt(0) - '0';
+        int digits = up.charAt(0) - '0'; // this will convert '2' into 2
+
         List<String> ans = new ArrayList<>();
 
         for(int i=(digits-1)*3;i<digits*3;i++){
@@ -23,4 +42,20 @@ public class LetterCombination {
         }
         return ans;
     }
+
+
+    //Count of letter combination
+    static int padCount(String p, String up) {
+        if (up.isEmpty()) {
+            return 1;
+        }
+        int count = 0;
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            count = count + padCount(p + ch, up.substring(1));
+        }
+        return count;
+    }
+
 }
