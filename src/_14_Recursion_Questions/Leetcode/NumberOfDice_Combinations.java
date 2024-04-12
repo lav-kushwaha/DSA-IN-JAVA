5c23929b189630b1686e8b1918200ebdff3f97ed
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-13 00:43:49 +0530

    Number Of Dice Combinations

--- src/_14_Recursion_Questions/Leetcode/NumberOfDice_Combinations.java
+++ src/_14_Recursion_Questions/Leetcode/NumberOfDice_Combinations.java
@@ -1,12 +1,14 @@
 package _14_Recursion_Questions.Leetcode;
 import java.util.ArrayList;
 
-public class NumberOfDice_Combination {
+public class NumberOfDice_Combinations {
     public static void main(String[] args) {
 //        DicePrint("",4);
         System.out.println(Dice("",4));
 //        System.out.println(CountDice("",4));
     }
+
+    //Dice Print.
     static void DicePrint(String p,int target){
         if(target==0){
             System.out.print(p+" ");
@@ -17,7 +19,7 @@ public class NumberOfDice_Combination {
         }
     }
 
-
+    //Dice.
     static ArrayList<String> Dice(String p,int target){
         if(target==0){
             ArrayList<String> list = new ArrayList<>();
@@ -31,6 +33,7 @@ public class NumberOfDice_Combination {
         return list;
     }
 
+    //count dice.
     static int CountDice(String p,int target){
         if(target==0){
             return 1;
@@ -40,5 +43,31 @@ public class NumberOfDice_Combination {
             count = count + CountDice(p+i,target-i);
         }
         return count;
+    }
+
+    //Dice Face.
+    static void diceFace(String p, int target, int face) {
+        if (target == 0) {
+            System.out.println(p);
+            return;
+        }
+
+        for (int i = 1; i <= face && i <= target; i++) {
+            diceFace(p + i, target - i, face);
+        }
+    }
+
+    //DiceFace Return.
+    static ArrayList<String> diceFaceRet(String p, int target, int face) {
+        if (target == 0) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 1; i <= face && i <= target; i++) {
+            list.addAll(diceFaceRet(p + i, target - i, face));
+        }
+        return list;
     }
 }
