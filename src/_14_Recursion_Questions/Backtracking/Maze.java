aa68663d89b83f78d2327bde3d44a6d0fe0d5071
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-14 00:43:55 +0530

    Maze Problems

--- src/_14_Recursion_Questions/Backtracking/Maze.java
+++ src/_14_Recursion_Questions/Backtracking/Maze.java
@@ -1,12 +1,16 @@
 package _14_Recursion_Questions.Backtracking;
 
 import java.util.ArrayList;
+//https://leetcode.com/problems/unique-paths/description/
+//62. Unique Paths
 
 public class Maze {
     public static void main(String[] args) {
-        System.out.println(count(3,3));
-         path("",3,3);
+//        System.out.println(count(3,3));
+//         path("",3,3);
         System.out.println(pathRet("",3,3));
+
+        System.out.println(pathRetDiagonal("",3,3));
     }
 
     //count the path.
@@ -55,6 +59,29 @@ public class Maze {
         }
         if(col>1) {
             ans.addAll(pathRet(p+"R",row,col-1));
+        }
+        return ans;
+    }
+
+    //path retun in arraylist.
+    public static ArrayList<String> pathRetDiagonal (String p,int row, int col){
+        //when any of row and col becomes 1 just return 1.
+        if(row == 1 && col==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> ans = new ArrayList<>();
+        if(row>1 && col>1){
+            ans.addAll(pathRetDiagonal(p+"D",row-1,col-1));
+        }
+
+        if(row>1){
+            ans.addAll(pathRetDiagonal(p+"V",row-1,col));
+        }
+        if(col>1) {
+            ans.addAll(pathRetDiagonal(p+"H",row,col-1));
         }
         return ans;
     }
