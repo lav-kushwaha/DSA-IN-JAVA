1cb79494554bdeddc5139cc900c5bcc9edcc2bca
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-17 04:59:53 +0530

    N Queens

--- src/_14_Recursion_Questions/Backtracking/Backtracking_Questions/N_Queens.java
+++ src/_14_Recursion_Questions/Backtracking/Backtracking_Questions/N_Queens.java
@@ -32,14 +32,15 @@ public class N_Queens {
 
     private static boolean isSafe(boolean[][] board, int row, int col) {
 
-        // Check if there is no queen in the same column
+        // Check if there is no queen in the same column.
+        // or for checking vertical row.
         for (int i = 0; i < row; i++) {
             if (board[i][col]) {
                 return false;
             }
         }
 
-        // Check the diagonal on the left side
+        // Check the diagonal on the left side.
         int maxLeft = Math.min(row, col);
         for (int i = 1; i <= maxLeft; i++) {
             if (board[row - i][col - i]) {
@@ -47,27 +48,27 @@ public class N_Queens {
             }
         }
 
-        // Check the diagonal on the right side
-        int maxRight = Math.min(row, board.length - col - 1); // we are doing col-1 because index starts from 0 and we want to check the right side.
+        // Check the diagonal on the right side.
+        int maxRight = Math.min(row, board.length - col - 1); // we are doing col-1 because index starts from 0 and we want to check the right side diagonal.
         for (int i = 1; i <= maxRight; i++) {
             if (board[row - i][col + i]) {
                 return false;
             }
         }
 
-        // If no conflicts, it's safe to place the queen in this cell
+        // If no conflicts, it's safe to place the queen in this cell.
         return true;
     }
 
     //display
     private static void display(boolean[][] board) {
-        // Display the current configuration of the board
+        // Display the current configuration of the board.
         for (boolean[] row : board) {
             for (boolean element : row) {
                 if (element) {
-                    System.out.print("Q "); // Print 'Q' if the cell is occupied by a queen
+                    System.out.print("Q "); // Print 'Q' if the cell is occupied by a queen.
                 } else {
-                    System.out.print("X "); // Print 'X' if the cell is empty
+                    System.out.print("X "); // Print 'X' if the cell is empty.
                 }
             }
             System.out.println();
