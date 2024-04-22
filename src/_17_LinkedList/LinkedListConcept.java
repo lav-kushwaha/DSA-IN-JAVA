20325edfed402140e204d0764ca0d6049cf95736
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-22 14:38:40 +0530

    Linked List

---
+++ src/_17_LinkedList/LinkedListConcept.java
@@ -0,0 +1,12 @@
+package _17_LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListConcept {
+    public static void main(String[] args) {
+        //Generics Linked-list provided by java to us.
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(12);
+        System.out.println(list);//[12]
+    }
+}
