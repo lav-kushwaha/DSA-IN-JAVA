cbb8d78e056f6342d0d03a552d0e8be25418ef76
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-29 21:59:31 +0530

    Count of Node element

--- src/_17_LinkedList/LinkedListConcept.java
+++ src/_17_LinkedList/LinkedListConcept.java
@@ -6,24 +6,44 @@ public class LinkedListConcept {
     private Node head; // Head of the linked list.
     private Node tail; // Tail of the linked list.
 
-    private int size;
+    private int size; // Size of the linked list.
 
-    // Constructor.
+    // Node - which has a value and next node.
+    // Private because we don't want to access anyone.
+    private class Node {
+
+        private int value; // Value of the node.
+        private Node next; // Reference to the next node. By default, it is null
+
+//        // Constructor with value parameter.
+        public Node(int value) {
+            this.value = value;
+        }
+
+        // Constructor with value and next node parameters.
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    // Constructor of the LinkedListConcept class.
     public LinkedListConcept() {
-
+        // Initialize size to 0.
         this.size = 0;
     }
 
     // Method to add a new node at the beginning of the linked list.
     public void insertFirst(int val) {
-        // Inserting new node
+        // Create a new node with the given value.
         Node newNode = new Node(val);
-        // New node pointing to the next node, but the problem is now head position is 2nd.
+        // Set the next pointer of the new node to the current head.
         newNode.next = head;
-        // Here head comes to the first position.
+        // Update the head to point to the new node.
         head = newNode;
 
         // If the list was empty, update the tail reference as well.
+        //Head and tail pointing to the same node.
         if (tail == null) {
             tail = head;
         }
@@ -31,45 +51,41 @@ public class LinkedListConcept {
         // Increase the size.
         size++;
     }
-    // Method to print all elements of the linked list
+
+    // Method to print all elements of the linked list.
     public void display() {
+        // Start from the head of the list.
         Node temp = head;
 
+        // Traverse the list and print each element.
         while (temp != null) {
             System.out.print(temp.value + " -> ");
             temp = temp.next;
         }
+
+        // Print "END" to mark the end of the list.
         System.out.println("END");
     }
 
-    // Node - which has a value and next node.
-    // Private because we don't want to access anyone.
-    private class Node {
-
-        private int value; // By default node value is null.
-        private Node next; // By default node value is null.
-
-        // Constructor.
-        public Node(int value) {
-            this.value = value;
-        }
-
-        //Constructor.
-        public Node(int value, Node next) {
-            this.value = value;
-            this.next = next;
-        }
+    // Method to print the count of elements in the linked list.
+    public void printCount() {
+        System.out.println("Count: " + size);
     }
-//    public static void main(String[] args) {
-//        // Creating a new linked list.
-//        LinkedListConcept linkedList = new LinkedListConcept();
-//        // Adding some elements to the list
-//        linkedList.insertFirst(3);
-//        linkedList.insertFirst(5);
-//        linkedList.insertFirst(7);
-//        linkedList.insertFirst(9);
-//
-//        // Displaying the linked list
-//        linkedList.display();
-//    }
+
+    // Main method to test the LinkedListConcept class.
+    public static void main(String[] args) {
+        // Creating a new linked list.
+        LinkedListConcept linkedList = new LinkedListConcept();
+        // Adding some elements to the list
+        linkedList.insertFirst(3);
+        linkedList.insertFirst(5);
+        linkedList.insertFirst(7);
+        linkedList.insertFirst(9);
+
+        // Displaying the linked list
+        linkedList.display();
+
+        // Print the count of elements
+        linkedList.printCount();
+    }
 }
