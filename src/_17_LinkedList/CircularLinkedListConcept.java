d4206932283732ea594077c5a50fbb3fafac9457
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-03 22:39:03 +0530

    Cicular Linked List

--- src/_17_LinkedList/CircularLinkedListConcept.java
+++ src/_17_LinkedList/CircularLinkedListConcept.java
@@ -1,8 +1,54 @@
 package _17_LinkedList;
 
-public class CircularLinkedListConcept {
-    public static void main(String[] args) {
+import java.util.SortedMap;
 
+public class CircularLinkedListConcept {
+
+    private Node head;
+    private Node tail;
+
+    class Node {
+        private int val;
+        private Node next;
+
+        Node(int val) {
+            this.val = val;
+            this.next = null;
+        }
     }
 
+    public void insert(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            tail.next = head; // Circular connection
+        } else {
+            tail.next = newNode; // Connect new node to the end
+            tail = newNode; // Update tail to the new node
+            tail.next = head; // Circular connection
+        }
+    }
+
+    public void display() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        Node current = head;
+        do {
+            System.out.print(current.val + " => ");
+            current = current.next;
+        } while (current != head);
+        System.out.println("Head");
+    }
+
+    public static void main(String[] args) {
+        CircularLinkedListConcept cll = new CircularLinkedListConcept();
+        cll.insert(12);
+        cll.insert(13);
+        cll.insert(14);
+        cll.display(); // Output: 12 => 13 => 14 =>
+    }
 }
