b71bf373aa056e4a3b08282ce70efa528dbe5b19
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-06 00:19:51 +0530

    Merge Two Sorted List

--- src/_17_LinkedList/LinkedListQuestions/MergeTwoSortedList.java
+++ src/_17_LinkedList/LinkedListQuestions/MergeTwoSortedList.java
@@ -2,24 +2,16 @@ package _17_LinkedList.LinkedListQuestions;
 //https://leetcode.com/problems/merge-two-sorted-lists/description/
 //21. Merge Two Sorted Lists
 
+
 public class MergeTwoSortedList {
     private ListNode head;
 
-    // Creating Node.
     class ListNode {
         int val;
         ListNode next;
 
-        ListNode() {
-        }
-
         ListNode(int val) {
             this.val = val;
-        }
-
-        ListNode(int val,ListNode next) {
-            this.val = val;
-            this.next = next;
         }
     }
 
@@ -46,9 +38,71 @@ public class MergeTwoSortedList {
         // Print "END" to mark the end of the list.
         System.out.println("END");
     }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // if list1 and list2 will be empty
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        // Merge list in separate node.
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        // check if any one of list is longer.
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+
     public static void main(String[] args) {
-        MergeTwoSortedList Merge = new MergeTwoSortedList();
-        Merge.display(); // Displaying the list after removing duplicates
+        MergeTwoSortedList merge = new MergeTwoSortedList();
+
+        // Populate list1: 1 -> 2 -> 4
+        merge.insertFirst(4);
+        merge.insertFirst(2);
+        merge.insertFirst(1);
+
+        // Display list1
+        System.out.println("List 1:");
+        merge.display();
+
+        // Create another instance for list2
+        MergeTwoSortedList merge2 = new MergeTwoSortedList();
+
+        // Populate list2: 1 -> 3 -> 4
+        merge2.insertFirst(4);
+        merge2.insertFirst(3);
+        merge2.insertFirst(1);
+
+        // Display list2
+        System.out.println("List 2:");
+        merge2.display();
+
+        // Merge the two lists
+        ListNode mergedList = merge.mergeTwoLists(merge.head, merge2.head);
+
+        // Display the merged list
+        System.out.println("Merged List:");
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " -> ");
+            mergedList = mergedList.next;
+        }
+        System.out.println("END");
     }
 }
 
