b9ae603e8e749f1f641a2e123abe2f3e6f9a68a3
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-07 03:49:06 +0530

    Happy Number

---
+++ src/_17_LinkedList/LinkedListQuestions/CycleDetectionQuestions/HappyNumber.java
@@ -0,0 +1,32 @@
+package _17_LinkedList.LinkedListQuestions.CycleDetectionQuestions;
+
+//https://leetcode.com/problems/happy-number/
+//202. Happy Number
+
+class HappyNumber {
+
+   public static int digitSquareSum(int num) {
+        int sum = 0;
+        while (num != 0) {
+           int rem = num % 10;
+            sum += rem * rem;
+            num /= 10;
+        }
+        return sum;
+    }
+    
+    public static boolean isHappy(int num) {
+        int slow = num;
+        int fast = num;
+
+        do {
+            slow = digitSquareSum(slow); //move one step.
+            fast = digitSquareSum(digitSquareSum(fast)); //move two step.
+        } while (slow != fast);
+        return slow == 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isHappy(2));
+    }
+}
