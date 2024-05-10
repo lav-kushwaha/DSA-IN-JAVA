cdccb00a6b77fa5498ec9f14033a3a71a012bf50
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-10 23:42:27 +0530

    AddDigits

--- src/Questions/AddDigits.java
+++ src/Questions/AddDigits.java
@@ -1,8 +1,12 @@
 package Questions;
+
 //https://leetcode.com/problems/add-digits/
 //258. Add Digits
+
 class AddDigits {
+
     public static int addDigits(int num) {
+        // Repeat until the number becomes a single digit
         while (num >= 10) {
             num = digitsSum(num);
         }
@@ -11,14 +15,16 @@ class AddDigits {
 
     static int digitsSum(int num) {
         int sum = 0;
+        // Loop through each digit of the number
         while (num != 0) {
-            sum += num % 10;
-            num /= 10;
+            sum += num % 10; // Add the last digit to the sum
+            num /= 10;       // Remove the last digit from the number
         }
         return sum;
     }
 
     public static void main(String[] args) {
+        // Test the addDigits method with an example input
         System.out.println(addDigits(38));
     }
 }
