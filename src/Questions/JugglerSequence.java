9f0c7f285aa4f6c1e364d42f96f1b93381e0c11e
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-11 15:28:26 +0530

    JugglerSequence

---
+++ src/Questions/JugglerSequence.java
@@ -0,0 +1,34 @@
+package Questions;
+
+//https://www.geeksforgeeks.org/problems/juggler-sequence3930/1
+//Juggler Sequence.
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JugglerSequence {
+    public static List<Long> recursiveFunction(long n) {
+        List<Long> arr = new ArrayList<>();
+        arr.add(n);
+        return recursiveHelper(n, arr);
+    }
+
+    private static List<Long> recursiveHelper(long n, List<Long> arr) {
+        if (n <= 1) {
+            return arr;
+        }
+        if (n % 2 == 0) {
+            n = (long) Math.pow(n, (float) 1 / 2);
+            arr.add(n);
+        } else {
+            n = (long) Math.pow(n, (float) 3 / 2);
+            arr.add(n);
+        }
+        return recursiveHelper(n, arr);
+    }
+
+    public static void main(String[] args) {
+        List<Long> result = recursiveFunction(9); // Change the argument as needed
+        System.out.println(result);
+    }
+}
