3235be91f9e65ee8d71a251350fe0cd1b4fe417b
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-19 23:24:38 +0530

    Sum of series

---
+++ src/Questions/SumOfSeries.java
@@ -0,0 +1,13 @@
+package Questions;
+
+class SumOfSeries {
+    public static long seriesSum(int n) {
+        // code here
+         return (long)(n)*(n+1)/2;
+    }
+
+    public static void main(String[] args) {
+        long ans = seriesSum(10);
+        System.out.println(ans);
+    }
+}
