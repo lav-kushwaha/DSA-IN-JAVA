fb88c0db6c7b1aa36fc5faa9529a1ad504d4d466
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-22 01:43:06 +0530

    StackMain

--- src/_18_Stacks_And_Queue/CustomStacks/StackMain.java
+++ src/_18_Stacks_And_Queue/CustomStacks/StackMain.java
@@ -1,2 +1,19 @@
-package _18_Stacks_And_Queue.CustomStacks;public class StackMain {
+package _18_Stacks_And_Queue.CustomStacks;
+
+public class StackMain {
+    public static void main(String[] args) throws StackException {
+        CustomStacks stack = new CustomStacks(5);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+
+        // Pop elements from the stack.
+        System.out.println(stack.pop());  // Output: 40
+        System.out.println(stack.pop());  // Output: 30
+        System.out.println(stack.pop());  // Output: 20
+        System.out.println(stack.pop());  // Output: 10
+
+//        System.out.println(stack.pop());//Exception in thread : Cannot pop from an empty stack!!
+    }
 }
