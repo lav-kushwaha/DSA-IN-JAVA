7a973b8593429a858279a3494671c0c385213276
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-22 01:42:38 +0530

    Stack Exception

--- src/_18_Stacks_And_Queue/CustomStacks/StackException.java
+++ src/_18_Stacks_And_Queue/CustomStacks/StackException.java
@@ -1,2 +1,11 @@
-package _18_Stacks_And_Queue.CustomStacks;public class StackException {
+package _18_Stacks_And_Queue.CustomStacks;
+
+//creating seprated class for Exception
+public class StackException extends Exception { //class StackException that extends the built-in Exception class.
+
+    public StackException(String message){
+        // Constructor that takes a message and passes it to the parent Exception class
+       super(message);
+    }
+
 }
