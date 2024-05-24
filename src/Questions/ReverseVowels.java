2385d6892791ad8d0e692694ea8407819d4051af
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-24 22:55:24 +0530

    ReverseVowels

---
+++ src/Questions/ReverseVowels.java
@@ -0,0 +1,35 @@
+package Questions;
+
+class ReverseVowels
+{
+    static String modify(String s)
+    {
+         // your code here
+        char[] chars = s.toCharArray();
+        int i = 0, j = chars.length - 1;
+        String vowels = "aeiouAEIOU";
+
+        while (i < j) {
+            while (i < j && !vowels.contains(chars[i] + ""))
+                i++;
+            while (i < j && !vowels.contains(chars[j] + ""))
+                j--;
+
+            if (i < j) {
+                char temp = chars[i];
+                chars[i] = chars[j];
+                chars[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        String ans = modify("Lavkushwaha");
+        System.out.println(ans);
+
+    }
+}
