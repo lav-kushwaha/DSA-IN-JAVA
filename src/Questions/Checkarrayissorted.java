1465a9d9774a3d82ba96a461d70d8440600fed6f
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-29 23:12:47 +0530

    Check Array Sorted

---
+++ src/Questions/Checkarrayissorted.java
@@ -0,0 +1,19 @@
+package Questions;
+
+class Checkarrayissorted {
+    static boolean arraySortedOrNot(int[] arr, int n) {
+         // code here
+        for(int i = 0; i < n - 1; i++){
+             if(arr[i] > arr[i+1]){
+            return false;
+            }
+       }
+       return true;
+    }
+
+     public static void main(String[] args) {
+         int arr[] = {10, 20, 30, 40, 50};
+         int N = 5;
+         System.out.println(arraySortedOrNot(arr,N));
+     }
+}
