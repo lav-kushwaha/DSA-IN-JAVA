717bf1a09b06980f0daed4716be72a2f50d94451
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-29 23:06:21 +0530

    Circular Queue

--- src/_18_Stacks_And_Queue/CircularQueue/CircularQueue.java
+++ src/_18_Stacks_And_Queue/CircularQueue/CircularQueue.java
@@ -1,4 +1,5 @@
-package _18_Stacks_And_Queue.CustomQueue;
+package _18_Stacks_And_Queue.CircularQueue;
+
 
 public class CircularQueue {
     private int[] data;
@@ -19,7 +20,7 @@ public class CircularQueue {
 
     // Check if the queue is full
     public boolean isFull() {
-        return size == data.length;
+        return size == data.length; //here, we have not taken end bcz end will may come to zero index,so take size.
     }
 
     // Check if the queue is empty
@@ -33,8 +34,15 @@ public class CircularQueue {
             System.out.println("Queue is full");
             return false;
         }
-        data[end++] = item;
+
+        data[end++] = item; //Insert element and then increment by 1.
+
+        // Forming the circular loop: if end reaches the length of the data array, reset it to 0
+        //if end and data.length will be same then it will make circular queue end will go to zeroth index.
+        //ex - end = 5%5 = 0th
         end = end % data.length;
+
+        //size of the array.
         size++;
         return true;
     }
@@ -44,8 +52,16 @@ public class CircularQueue {
         if (isEmpty()) {
             throw new Exception("Queue is empty");
         }
+
+        //Remove the item at the front position and then increment front
         int removed = data[front++];
+
+        // Forming the circular loop: if end reaches the length of the data array, reset it to 0
         front = front % data.length;
+
+        //Here, we have decreases size of the queue bcz if you don't decrease size and when size is equal to data.length it will throw queue is full.
+        //so, whenever we remove element we will decrease the size. so, that we can insert new element.
+        //Decrement the size of the queue.
         size--;
         return removed;
     }
@@ -55,7 +71,7 @@ public class CircularQueue {
         if (isEmpty()) {
             throw new Exception("Queue is empty");
         }
-        return data[front];
+        return data[front];// Return the item at the front position without removing it
     }
 
     // Display the elements of the queue
@@ -68,14 +84,14 @@ public class CircularQueue {
         int count = size;
         while (count > 0) {
             System.out.print(data[i] + " <- ");
-            i = (i + 1) % data.length;
+            i = (i + 1) % data.length;// Move to the next position in circular manner
             count--;
         }
         System.out.println("END");
     }
 
     public static void main(String[] args) throws Exception {
-        CustomQueue queue = new CustomQueue(5);
+        CircularQueue queue = new CircularQueue(5); // Instantiate the CircularQueue with size 5
 
         queue.insert(10);
         queue.insert(20);
