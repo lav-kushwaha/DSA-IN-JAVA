38b5503503698ac1b1f7ee89040fe505dc7267a5
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-30 03:06:37 +0530

    Queue Main

--- src/_18_Stacks_And_Queue/CustomQueue/QueueMain.java
+++ src/_18_Stacks_And_Queue/CustomQueue/QueueMain.java
@@ -4,15 +4,15 @@ public class QueueMain {
     public static void main(String[] args) throws Exception {
         CustomQueue queue = new CustomQueue(5);
 
-        queue.insert(10);
-        queue.insert(20);
-        queue.insert(30);
-        queue.insert(40);
-        queue.insert(50);
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+        queue.Enqueue(30);
+        queue.Enqueue(40);
+        queue.Enqueue(50);
 
         queue.display(); // Should display the elements in the queue
 
-        System.out.println(queue.remove() + " removed from queue");
+        System.out.println(queue.Dequeue() + " removed from queue");
         queue.display(); // Should display the remaining elements
 
         System.out.println("Front item is " + queue.front());
