b03b276ae450fa028c72c71aca09d1a942eea8be
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-30 03:06:03 +0530

    Dynamic Queue

--- src/_18_Stacks_And_Queue/CustomQueue/DynamicQueue.java
+++ src/_18_Stacks_And_Queue/CustomQueue/DynamicQueue.java
@@ -1,16 +1,36 @@
-package _18_Stacks_And_Queue.CircularQueue;
+package _18_Stacks_And_Queue.CustomQueue;
 
-public class DynamicCircularQueue extends CircularQueue{
+public class DynamicQueue extends CustomQueue {
 
     // Default constructor
-    public DynamicCircularQueue() {
-        super();//using keyword it will call parent constructor CircularQueue(). ctrl+click on super to see.
+    public DynamicQueue() {
+        super();//using super keyword, it will call parent constructor CustomQueue(). ctrl+click on super() to see.
     }
 
     // Constructor with custom size
-    public DynamicCircularQueue(int size) {
-        super(size);//it will call parent constructor CircularQueue(int size)
+    public DynamicQueue(int size) {
+        super(size);//it will call parent constructor CustomQueue(int size)
     }
 
-    
+    // Insert an item to the queue
+    public boolean Enqueue(int item) {
+        if (isFull()) {
+            //double the array size.
+            int [] temp = new int[data.length*2];
+
+            //copy all previous item in new data.
+            for (int i=0;i<data.length;i++){
+                temp[i] = data[(front+i) % data.length];
+            }
+
+            front = 0;
+            end = data.length;
+            //temp are assign to the data.
+            data = temp;
+        }
+
+        return super.Enqueue(item);
+    }
+
+
 }
