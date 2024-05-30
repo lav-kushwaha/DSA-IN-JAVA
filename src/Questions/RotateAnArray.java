867f2b233e9ea6f5382b58de45576e147757394c
Lav Kushwaha <lavkumar1224@gmail.com>
2024-05-30 23:29:31 +0530

    Rotate An Array

--- src/Questions/RotateAnArray.java
+++ src/Questions/RotateAnArray.java
@@ -6,12 +6,16 @@ package Questions;
 import java.util.Arrays;
 
 class RotateAnArray {
+
      static void leftRotate(int[] arr, int n, int d) {
+
         d=d%n;
         reverse(arr,0,d-1);
         reverse(arr,d,n-1);
         reverse(arr,0,n-1);
     }
+
+    //Reverse method.
     public static void reverse(int arr[], int start, int end){
         while(start<end){
             int temp = arr[start];
