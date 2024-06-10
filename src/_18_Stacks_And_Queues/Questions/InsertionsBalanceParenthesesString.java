4ad13ca81116e6c447797b39c46c34ee3942def8
Lav Kushwaha <lavkumar1224@gmail.com>
2024-06-10 16:35:21 +0530

    Minimum Insertions to Balance a Parentheses String

---
+++ src/_18_Stacks_And_Queues/Questions/InsertionsBalanceParenthesesString.java
@@ -0,0 +1,40 @@
+package _18_Stacks_And_Queues.Questions;
+import java.util.Stack;
+
+//1541. Minimum Insertions to Balance a Parentheses String
+//https://leetcode.com/problems/minimum-insertions-to-balance-a-parentheses-string/description/
+
+public class InsertionsBalanceParenthesesString {
+    public static void main(String[] args) {
+        String s = "))())(";
+        System.out.println(minInsertions(s));
+    }
+    public static int minInsertions(String s) {
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch == '('){
+                stack.push(ch);
+            }
+            else{
+                if(i+1<s.length() && s.charAt(i+1)==')'){
+                    i++;
+                }else{
+                    count++;
+                }
+
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }else{
+                    count++;
+                }
+            }
+        }
+        count+=2*stack.size();
+        return count;
+        }
+    }
+
