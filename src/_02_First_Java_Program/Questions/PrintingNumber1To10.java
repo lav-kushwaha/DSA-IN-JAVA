6885915e691a481c560addaa385ddca0844643ea
Lav Kushwaha <89603685+lav-kushwaha@users.noreply.github.com>
2024-07-07 22:18:43 +0530

    PrintingNumber1To10

--- src/_02_First_Java_Program/Questions/PrintingNumber1To10.java
+++ src/_02_First_Java_Program/Questions/PrintingNumber1To10.java
@@ -3,7 +3,7 @@ package _02_First_Java_Program.Questions;
 public class PrintingNumber1To10 {
     public static void printNos(int N){
         if(N>0){
-            printNos(N-1);
+            printNos(N-1);//recursive call
             System.out.print(N+" ");
         }
     }
