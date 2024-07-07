8b1c545f17239db7f6cdc48073b786cf05ddfd2c
Lav Kushwaha <89603685+lav-kushwaha@users.noreply.github.com>
2024-07-07 22:17:13 +0530

    Inputs.java

--- src/_02_First_Java_Program/Inputs.java
+++ src/_02_First_Java_Program/Inputs.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Inputs {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);//Taking input from the user.
         int rollno = sc.nextInt();
         System.out.println("Your roll no is "+ rollno);
     }
