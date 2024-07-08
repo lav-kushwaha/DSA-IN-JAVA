70982f013587e73200761ff3cc2bf626dd5920f7
Lav Kushwaha <lavkumar1224@gmail.com>
2024-07-08 18:13:39 +0530

    Segment Tree

--- src/_19_Binary_Trees/SegmentTree.java
+++ src/_19_Binary_Trees/SegmentTree.java
@@ -14,77 +14,133 @@ public class SegmentTree {
         }
     }
 
-    //root node
+    // root node
     private Node root;
 
     public SegmentTree(int[] arr){
-        //create a tree using this array
-        this.root = constructTree(arr,0,arr.length-1);
+        if (arr.length > 0) {
+            this.root = constructTree(arr, 0, arr.length - 1);
+        }
     }
 
-    //construct tree.
+    // construct tree
     private Node constructTree(int[] arr, int start, int end) {
-        if(start == end){ //base condtion leaf node.
-            //leaf node
-            Node leaf = new Node(start,end);
+        if (start == end) { // base condition leaf node
+            Node leaf = new Node(start, end);
             leaf.data = arr[start];
             return leaf;
         }
 
-        //create new node with index you are at.
-        Node node = new Node(start,end);
+        Node node = new Node(start, end);
 
-        int mid = (start+end)/2;
+        int mid = (start + end) / 2;
 
-        node.left = this.constructTree(arr,start,mid);
-        node.right = this.constructTree(arr,mid+1,end);
-
+        node.left = this.constructTree(arr, start, mid);
+        node.right = this.constructTree(arr, mid + 1, end);
 
         node.data = node.left.data + node.right.data;
 
-        return node; //this node will return after addition from where that function is called.
+        return node;
     }
 
-
-    //display Method
+    // display method
     public void display(){
-        display(this.root);
+        if (this.root != null) {
+            display(this.root);
+        }
     }
 
     private void display(Node node){
         String str = "";
 
-        if(node.left!=null){
-            str = str + "Interval=[" + node.left.startInterval + "-" + node.left.startInterval+ "] and data: "+node.left.data+" => ";
-        }else{
-            str = str + "No left child";
+        if (node.left != null) {
+            str += "Interval=[" + node.left.startInterval + "-" + node.left.endInterval + "] and data: " + node.left.data + " => ";
+        } else {
+            str += "No left child => ";
         }
 
-        //for current node
-        str = str + "Interval=[" + node.startInterval + "-" + node.left.startInterval + "] and data: "+node.data+ " <=";
+        str += "Interval=[" + node.startInterval + "-" + node.endInterval + "] and data: " + node.data + " <= ";
 
-        if(node.right!=null){
-            str = str + "Interval=[" + node.right.startInterval + "-" + node.right.startInterval+ "] and data: "+node.right.data+" => ";
-        }else{
-            str = str + "No right child";
+        if (node.right != null) {
+            str += "Interval=[" + node.right.startInterval + "-" + node.right.endInterval + "] and data: " + node.right.data;
+        } else {
+            str += "No right child";
         }
 
         System.out.println(str);
 
-        //call recursion
-        if(node.left!=null){
+        if (node.left != null) {
             display(node.left);
         }
 
-        if(node.right!=null){
+        if (node.right != null) {
             display(node.right);
         }
     }
 
-    public static void main(String [] args){
-        int[] arr = {3,8,6,7,-2,-8,4,9};
+    // query
+    public int query(int qsi, int qei){
+        return this.query(this.root, qsi, qei);
+    }
 
+    private int query(Node node, int qsi, int qei){
+        if (node == null) {
+            return 0;
+        }
+
+        // case 1: node is completely inside the query range
+        if (node.startInterval >= qsi && node.endInterval <= qei) {
+            return node.data;
+        }
+
+        // case 2: node is completely outside the query range
+        if (node.endInterval < qsi || node.startInterval > qei) {
+            return 0;
+        }
+
+        // case 3: node is partially inside and partially outside the query range
+        return this.query(node.left, qsi, qei) + this.query(node.right, qsi, qei);
+    }
+
+    public void update(int index, int value){
+        if (this.root != null) {
+            update(this.root, index, value);
+        }
+    }
+
+    private int update(Node node, int index, int value){
+        if (node == null) {
+            return 0;
+        }
+
+        // case 1: index is within the node's interval
+        if (index >= node.startInterval && index <= node.endInterval) {
+            // base condition: leaf node
+            if (node.startInterval == node.endInterval) {
+                node.data = value;
+                return node.data;
+            }
+
+            int leftAns = update(node.left, index, value);
+            int rightAns = update(node.right, index, value);
+            node.data = leftAns + rightAns;
+            return node.data;
+        }
+
+        return node.data;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {3, 8, 6, 7, -2, -8, 4, 9};
         SegmentTree tree = new SegmentTree(arr);
+        tree.display();
+        System.out.println("Query (1, 6): " + tree.query(1, 6)); //sum of element between the range [1,6] is 15, 1 and 6 are index.
 
-   }
+
+        tree.update(3, 10);
+        System.out.println("After update: ");
+        tree.display();
+
+        System.out.println("Query (1, 6): " + tree.query(1, 6)); //after update sum of between the range become 15 to 18
+    }
 }
