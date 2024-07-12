2cb3c30719560cfd70c2999674c2b5ef6e8d09d8
Lav Kushwaha <lavkumar1224@gmail.com>
2024-07-13 01:38:56 +0530

    Random String Generate

--- src/_20_StringBuffer/RandomString.java
+++ src/_20_StringBuffer/RandomString.java
@@ -1,2 +1,20 @@
-package _20_StringBuffer;public class RandomString {
+package _20_StringBuffer;
+import java.util.Random;
+
+public class RandomString {
+    static String generate(int size){
+        StringBuffer sb = new StringBuffer(size); //it will not create new object individually everytime it will make change that only.
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+           int randomChar = 97+(int)(random.nextFloat()*26);
+           sb.append((char)randomChar+" ");
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generate(26));
+    }
 }
