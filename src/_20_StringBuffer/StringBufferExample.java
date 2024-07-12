65d5e5925356b7e4f66a6ab8c7d88a51e3d64a6e
Lav Kushwaha <lavkumar1224@gmail.com>
2024-07-12 21:58:13 +0530

    Different Constructor in String Buffer

--- src/_20_StringBuffer/StringBufferExample.java
+++ src/_20_StringBuffer/StringBufferExample.java
@@ -2,10 +2,27 @@ package _20_StringBuffer;
 
 public class StringBufferExample {
     public static void main(String[] args) {
+
+        //constructor 1
         StringBuffer sb = new StringBuffer();
+
+        //constructor 2
+        StringBuffer sb2 = new StringBuffer("Lav Kushwaha");
+
+        //constructor 3
+        StringBuffer sb3 = new StringBuffer(30);
+
+        //Different methods :
         sb.append("Hello world");
 
-        System.out.println(sb);
+        //its appended and it's not created new object.
+//        sb.append("String buffer");
+
+//        sb.insert(11, " Lav ");
+
+//        sb.reverse();
+
+        sb.replace(0,5,"lav");
 
         String str = sb.toString();
         System.out.println(str);
