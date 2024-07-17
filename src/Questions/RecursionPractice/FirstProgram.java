bf214ba11ee5d5420a6aa535252f8640e9f4a440
Lav Kushwaha <lavkumar1224@gmail.com>
2024-07-17 16:40:53 +0530

    Recursion Question

--- src/Questions/RecursionPractice/FirstProgram.java
+++ src/Questions/RecursionPractice/FirstProgram.java
@@ -2,7 +2,8 @@ package Questions.RecursionPractice;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        print(1);
+//        print(1);
+        System.out.println(fib(4));
     }
 
     static void print(int n){
@@ -12,7 +13,14 @@ public class FirstProgram {
         }
 
         print(n+1);//recursive call
-        System.out.println(n); //print
+        System.out.println(n); //print the no
+    }
 
+    //fibonacci series using recursion.
+    static int fib(int n){
+        if(n<2){ //base conditon.
+            return n;
+        }
+        return fib(n-1)+fib(n-2);//recursive call
     }
 }
