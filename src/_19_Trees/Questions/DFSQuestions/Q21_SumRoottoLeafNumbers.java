7023b8855faa2f3c2695f14310a96fa1a298b4bb
Lav Kushwaha <lavkumar1224@gmail.com>
2024-07-27 21:10:06 +0530

    Sum Root To Leaf Numbers

--- src/_19_Trees/Questions/DFSQuestions/Q21_SumRoottoLeafNumbers.java
+++ src/_19_Trees/Questions/DFSQuestions/Q21_SumRoottoLeafNumbers.java
@@ -51,3 +51,30 @@ public class Q21_SumRoottoLeafNumbers {
         System.out.println("The sum of all root-to-leaf numbers is: " + result);  // Output should be 1026
     }
 }
+
+
+//2nd method
+//class Solution {
+//    int sum = 0;  // to store the total sum of all root-to-leaf numbers
+//
+//    public int sumNumbers(TreeNode root) {
+//        helper(root,0);  // start the helper function with an empty string
+//        return sum;  // return the total sum
+//    }
+//
+//    private void helper(TreeNode root, int num) {
+//        if (root == null) return;  // base case: if the current node is null, return
+//
+//        num = num * 10 + root.val;  // append the current node's value to the path string
+//
+//        // If the current node is a leaf, convert the path string to an integer and add it to sum
+//        if (root.left == null && root.right == null) {
+//            sum = sum + num;
+//            return;  // return since it's a leaf node
+//        }
+//
+//        // Recursively call the helper function for left and right children
+//        helper(root.left, num);
+//        helper(root.right, num);
+//    }
+//}
