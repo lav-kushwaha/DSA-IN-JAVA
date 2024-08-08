97a0faf6aa37ec54000c91ced79e8ffa478fc03b
Lav Kushwaha <lavkumar1224@gmail.com>
2024-08-08 12:33:31 +0530

    SwapKth

--- src/Questions/SwapKth.java
+++ src/Questions/SwapKth.java
@@ -1,7 +1,9 @@
 package Questions;
 
+import java.util.Arrays;
+
 class SwapKth {
-    void swapKth(int arr[], int N, int K) {
+   static void swapKth(int arr[], int N, int K) {
         if (K > 0 && K <= N) {
             // Swap Kth element from the beginning with Kth element from the end
             int temp = arr[K - 1];
@@ -10,4 +12,10 @@ class SwapKth {
         }
     }
 
+    public static void main(String[] args) {
+       int [] arr = {1,2,3,4,5,6};
+       int K = 2;
+       swapKth(arr,arr.length,K);
+       System.out.println(Arrays.toString(arr));
+    }
 }
