602fe1c197f8f7c970defea1baf3b8e337edaea6
Lav Kushwaha <lavkumar1224@gmail.com>
2024-09-04 23:19:06 +0530

    Party Of Couples

--- src/Questions/PartyOfCouples.java
+++ src/Questions/PartyOfCouples.java
@@ -2,14 +2,12 @@ package Questions;
 
 class PartyOfCouples{
     static int findSingle(int n, int arr[]){
-          // code here
         int single = 0;
         for(int i=0;i<n;i++){
             single = single ^ arr[i];
         }
         return single;
     }
-
     public static void main(String[] args) {
         int arr [] = {1, 2, 3, 2, 1};
         int n = 5;
