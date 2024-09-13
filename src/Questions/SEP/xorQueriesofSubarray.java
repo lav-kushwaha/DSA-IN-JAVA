b49f4e9ec5f2530400940cfd1237ad109ded9080
Lav Kushwaha <lavkumar1224@gmail.com>
2024-09-13 23:22:11 +0530

    XOR Queries of a Subarray

--- src/Questions/SEP/xorQueriesofSubarray.java
+++ src/Questions/SEP/xorQueriesofSubarray.java
@@ -1,25 +1,46 @@
 package Questions.SEP;
 
+import java.util.Arrays;
+
 class xorQueriesofSubarray {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        int [] cumXOR = new int[arr.length];
+        // Calculate cumulative XOR
+        int[] cumXOR = new int[arr.length];
         cumXOR[0] = arr[0];
 
-        for(int i=1;i<arr.length;i++){
-            cumXOR[i] = cumXOR[i-1] ^ arr[i];
+        for (int i = 1; i < arr.length; i++) {
+            cumXOR[i] = cumXOR[i - 1] ^ arr[i];
         }
 
-        int [] result = new int[arr.length];
+        int[] result = new int[queries.length];
         int i = 0;
-        for(int[] query : queries){
+
+        for (int[] query : queries) {
             int L = query[0];
             int R = query[1];
 
-            int xorVal = cumXOR[R] ^ (L==0?0:cumXOR[L-1]);
+            int xorVal = cumXOR[R] ^ (L == 0 ? 0 : cumXOR[L - 1]);
             result[i] = xorVal;
             i++;
         }
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        xorQueriesofSubarray solution = new xorQueriesofSubarray();
+
+        int[] arr = {1, 3, 4, 8};
+
+        int[][] queries = {
+                {0, 1},
+                {1, 2},
+                {0, 3},
+                {3, 3}
+        };
+
+        int[] result = solution.xorQueries(arr, queries);
+
+        System.out.println(Arrays.toString(result)); // Output: [2, 7, 14, 8]
     }
 }
