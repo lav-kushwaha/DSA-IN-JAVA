4835bcb38210f9e983298b440ccc46275e182a7a
Lav Kushwaha <lavkumar1224@gmail.com>
2024-09-17 23:18:40 +0530

    Find Min Time Difference

--- src/Questions/SEP/findMinTimeDifference.java
+++ src/Questions/SEP/findMinTimeDifference.java
@@ -1,16 +1,23 @@
 package Questions.SEP;
+
 import java.util.Arrays;
 import java.util.List;
 
-//https://leetcode.com/problems/minimum-time-difference/?envType=daily-question&envId=2024-09-16
+//https://leetcode.com/problems/minimum-time-difference/description/
 //539. Minimum Time Difference
 
 class findMinTimeDifference {
+
     public int findMinDifference(List<String> timePoints) {
         int n = timePoints.size();
+
+        // If there are more time points than the possible number of minutes in a day, return 0.
+        // This is because there must be duplicate times, leading to a difference of 0.
+        if (n > 1440) return 0;
+
         int[] minutes = new int[n];
 
-        // Convert timePoints to minutes
+        // Convert timePoints to minutes since 00:00
         for (int i = 0; i < n; i++) {
             String time = timePoints.get(i);
             String[] parts = time.split(":");
@@ -21,7 +28,7 @@ class findMinTimeDifference {
             minutes[i] = hours * 60 + mins;
         }
 
-        // Sort the minutes array
+        // Sort the minutes array to compare adjacent time points
         Arrays.sort(minutes);
 
         // Initialize result with the maximum possible value
@@ -36,5 +43,11 @@ class findMinTimeDifference {
         result = Math.min(result, (24 * 60 - minutes[n - 1]) + minutes[0]);
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        findMinTimeDifference obj = new findMinTimeDifference();
+        List<String> timePoints = Arrays.asList("23:59", "00:00", "12:30");
+        System.out.println("Minimum Time Difference: " + obj.findMinDifference(timePoints));  // Output: 1
     }
 }
