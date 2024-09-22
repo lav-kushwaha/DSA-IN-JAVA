d51ece638b94ea95b5de2c3eb99250b08180cd7d
Lav Kushwaha <lavkumar1224@gmail.com>
2024-09-22 23:37:56 +0530

    Adjacency List

---
+++ src/_30_Graph_Theory/Graph_Representations/Adjacency_List.java
@@ -0,0 +1,43 @@
+package _30_Graph_Theory.Graph_Representations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Adjacency_List {
+    
+    // Method to add an edge between two vertices
+    public static void addEdge(List<List<Integer>> adj, int i, int j) {
+        adj.get(i).add(j);
+        adj.get(j).add(i); // Undirected
+    }
+
+    // Method to display the adjacency list
+    public static void displayAdjList(List<List<Integer>> adj) {
+        for (int i = 0; i < adj.size(); i++) {
+            System.out.print(i + ": "); // Print the vertex
+            for (int j : adj.get(i)) {
+                System.out.print(j + " "); // Print its adjacent 
+            }
+            System.out.println(); 
+        }
+    }
+
+    public static void main(String[] args) {
+      
+        // Create a graph with 4 vertices and no edges
+        int V = 4;
+        List<List<Integer>> adj = new ArrayList<>(V); 
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Now add edges one by one
+        addEdge(adj, 0, 1);
+        addEdge(adj, 0, 2);
+        addEdge(adj, 1, 2);
+        addEdge(adj, 2, 3);
+
+        System.out.println("Adjacency List Representation:");
+        displayAdjList(adj);
+    }
+}
