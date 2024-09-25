68d379b34454fbdc0400af9b34e884e46cb01d52
Lav Kushwaha <lavkumar1224@gmail.com>
2024-09-26 00:22:21 +0530

    Brute force and Hasmaps approach

--- src/Questions/SEP/PrefixScore.java
+++ src/Questions/SEP/PrefixScore.java
@@ -1,9 +1,12 @@
 package Questions.SEP;
-
 import java.util.*;
 
+//https://leetcode.com/problems/sum-of-prefix-scores-of-strings/description/
+//2416. Sum of Prefix Scores of Strings
+
 public class PrefixScore {
-    
+
+    //Brute force approaches
     public static int[] sumPrefixScores(String[] words) {
         int n = words.length;
         int[] result = new int[n];
@@ -19,6 +22,34 @@ public class PrefixScore {
             result[i] = totalScore;
         }
         
+        return result;
+    }
+
+    //HashMaps approaches
+    public static int[] sumPrefixScores02(String[] words) {
+        Map<String, Integer> prefixCountMap = new HashMap<>();
+
+        for (String word : words) {
+            for (int prefixLength = 1; prefixLength <= word.length(); prefixLength++) {
+                String prefix = word.substring(0, prefixLength);
+                prefixCountMap.put(prefix, prefixCountMap.getOrDefault(prefix, 0) + 1);
+            }
+        }
+
+        int[] result = new int[words.length];
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            int totalScore = 0;
+
+            for (int prefixLength = 1; prefixLength <= word.length(); prefixLength++) {
+                String prefix = word.substring(0, prefixLength);
+                totalScore += prefixCountMap.get(prefix);
+            }
+
+            result[i] = totalScore;
+        }
+
         return result;
     }
     
