2f8701506bcbbe3b0fdb453d207e93b2947abf8b
Lav Kushwaha <lavkumar1224@gmail.com>
2024-09-27 23:29:41 +0530

    MyCalendar II

--- src/Questions/SEP/MyCalendarTwo.java
+++ src/Questions/SEP/MyCalendarTwo.java
@@ -1,8 +1,14 @@
 package Questions.SEP;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
+//https://leetcode.com/problems/my-calendar-ii/description/
+//731. My Calendar II
+
+//Line sweep algorithm
 class MyCalendarTwo {
     TreeMap<Integer, Integer> bookings;
     int maxOverAllowed;
@@ -44,4 +50,40 @@ class MyCalendarTwo {
         System.out.println(myCalendar.book(25, 55)); // true
     }
 }
+
+
+//second method - best
+//class MyCalendarTwo {
+//    List<int[]> bookings;
+//    TreeMap<Integer,Integer> overlappedMap;
+//    public MyCalendarTwo() {
+//        bookings = new ArrayList<>();
+//        overlappedMap = new TreeMap<>();
+//    }
+//
+//    public boolean book(int start, int end) {
+//        Integer prevVal = overlappedMap.lowerKey(end);
+//        if(prevVal!=null && start<=overlappedMap.get(prevVal)-1){
+//            return false;
+//        }
+//
+//        for(int [] booking : bookings){
+//            int commStart = Math.max(booking[0],start);
+//            int commEnd = Math.min(booking[1],end);
+//
+//            if(commStart<commEnd){
+//                overlappedMap.put(commStart,commEnd);
+//            }
+//        }
+//
+//        bookings.add(new int[]{start,end});
+//        return true;
+//    }
+//}
+//
+///**
+// * Your MyCalendarTwo object will be instantiated and called as such:
+// * MyCalendarTwo obj = new MyCalendarTwo();
+// * boolean param_1 = obj.book(start,end);
+// */
 
