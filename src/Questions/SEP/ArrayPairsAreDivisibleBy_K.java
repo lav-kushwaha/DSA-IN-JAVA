9cd10985022391b353b06433a3ede0f11f2fbfd2
Lav Kushwaha <lavkumar1224@gmail.com>
2024-10-01 23:42:40 +0530

    Arrays Pairs

---
+++ src/Questions/SEP/ArrayPairsAreDivisibleBy_K.java
@@ -0,0 +1,27 @@
+package Questions.SEP;
+
+public class ArrayPairsAreDivisibleBy_K {
+    public boolean canArrange(int[] arr, int k) {
+        int[] remainderCount = new int[k];
+
+        for (int num : arr) {
+            int remainder = ((num % k) + k) % k;
+            remainderCount[remainder]++;
+        }
+
+        for (int i = 1; i < k; i++) {
+            if (remainderCount[i] != remainderCount[k - i]) {
+                return false;
+            }
+        }
+
+        return remainderCount[0] % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        ArrayPairsAreDivisibleBy_K solution = new ArrayPairsAreDivisibleBy_K();
+        int[] arr1 = {1, 2, 3, 4, 5, 10, 6, 7, 8, 9};
+        int k1 = 5;
+        System.out.println(solution.canArrange(arr1, k1)); // Output: true
+    }
+}
