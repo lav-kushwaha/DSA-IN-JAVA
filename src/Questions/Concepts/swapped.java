9978c80040f960f9a2a507c38f26e0ce1205dc5e
Lav Kushwaha <lavkumar1224@gmail.com>
2024-10-03 23:46:31 +0530

    Swapped

--- src/Questions/Concepts/swapped.java
+++ src/Questions/Concepts/swapped.java
@@ -1,36 +1,35 @@
 package Questions.Concepts;
 
-public class swapped {
-    public static void main(String[] args) {
-        int[] arr = {10, 20};
-        swap(arr, 0, 1);
-        System.out.print(arr[0] + " " + arr[1]); // Output: 20 10
-    }
-
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-}
-
-//Note: Java passes arguments by value, not by reference.
-//class Swapped {
-//
+//public class swapped {
 //    public static void main(String[] args) {
-//        int a = 10;
-//        int b = 20;
-//        swap(a, b);
-//        System.out.print(a + " " + b);
+//        int[] arr = {10, 20};
+//        swap(arr, 0, 1);
+//        System.out.print(arr[0] + " " + arr[1]); // Output: 20 10
 //    }
 //
-//    public static void swap(int a, int b) {
-//        int temp = a;
-//        a = b;
-//        b = temp;
+//    public static void swap(int[] arr, int i, int j) {
+//        int temp = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = temp;
 //    }
 //}
+
+//Note: Java passes arguments by value, not by reference.
+class Swapped {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        swap(a, b);
+        System.out.print(a + " " + b);
+    }
+
+    public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+}
 
 //Note : please note that this implementation will not actually swap the values of a and b in the main method because
 // Java passes arguments by value, not by reference.
