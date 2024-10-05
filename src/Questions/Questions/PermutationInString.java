414c6f7d2d3921b888b08a3c708e187ba171529c
Lav Kushwaha <lavkumar1224@gmail.com>
2024-10-05 22:49:36 +0530

    Permutation In String

---
+++ src/Questions/Questions/PermutationInString.java
@@ -0,0 +1,33 @@
+package Questions.Questions;
+
+class PermutationInString {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        int [] arr1 = new int[26];
+
+        for(int i=0;i<n;i++){
+            arr1[s1.charAt(i) - 'a']++;
+        }
+
+        for(int i=0;i<=m-n;i++){
+            int [] arr2 = new int[26];
+            for(int j=0;j<n;j++){
+                arr2[s2.charAt(i+j) - 'a']++;
+            }
+            if(isMatch(arr1,arr2)) 
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isMatch(int[] arr1, int[] arr2){
+        for(int i=0;i<26;i++){
+            if(arr1[i]!=arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
