f77299f239137ce0ce73d8c5bba8442f39f3eb60
Lav Kushwaha <lavkumar1224@gmail.com>
2024-10-17 23:28:49 +0530

    Equilibrium Point

--- src/Questions/Questions/EquilibriumPoint.java
+++ src/Questions/Questions/EquilibriumPoint.java
@@ -4,14 +4,7 @@ package Questions.Questions;
 //https://www.geeksforgeeks.org/problems/equilibrium-point-1587115620/1?page=1&category=Arrays,Strings,Sorting,Matrix,Searching,Recursion,Binary%20Search&sortBy=submissions
 
 public class EquilibriumPoint {
-    public static void main(String[] args) {
-        int A[] = {1,3,5,2,2};
-        long ans = equilibriumPoint(A,A.length);
-        System.out.println(ans);
-    }
-    
     public static int equilibriumPoint(int[] a, int n) {
-        // Your code here
         long sum = 0;
         for (int i = 0; i < n; i++) {
             sum += a[i];
@@ -27,4 +20,10 @@ public class EquilibriumPoint {
         }
         return -1;// No equilibrium index found.
     }
+    public static void main(String[] args) {
+        int A[] = {1,3,5,2,2};
+        long ans = equilibriumPoint(A,A.length);
+        System.out.println(ans);
+    }
+
 }
