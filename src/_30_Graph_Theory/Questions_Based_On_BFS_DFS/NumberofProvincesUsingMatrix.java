a7cf5853b865ca00b429e7f1a409407195c8abad
Lav Kushwaha <lavkumar1224@gmail.com>
2024-11-02 15:55:38 +0530

    Number of provinces using matrix

---
+++ src/_30_Graph_Theory/Questions_Based_On_BFS_DFS/NumberofProvincesUsingMatrix.java
@@ -0,0 +1,30 @@
+package _30_Graph_Theory.Questions_Based_On_BFS_DFS;
+
+//https://leetcode.com/problems/number-of-provinces/
+//547. Number of Provinces
+
+//APPROACH : DFS
+class NumberofProvincesUsingMatrix {
+    public int findCircleNum(int[][] isConnected) {
+        int provinces = 0;
+        boolean[] visited = new boolean[isConnected.length];
+
+        for(int i = 0; i<isConnected.length;i++){
+            if(!visited[i]){
+                provinces++;
+                dfs(isConnected, visited, i);
+            }
+        }
+        return provinces;
+    }
+
+    private void dfs(int[][] isConnected, boolean[] visited, int i){
+        visited[i] = true;
+
+        for(int j=0;j<isConnected.length;j++){
+            if(isConnected[i][j]==1 && !visited[j]){
+                dfs(isConnected,visited,j);
+            }
+        }
+    }
+}
