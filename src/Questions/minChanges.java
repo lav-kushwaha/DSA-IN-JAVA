0dd7d5dc42d0ac67cfff03088fa746876f6ddbda
Lav Kushwaha <lavkumar1224@gmail.com>
2024-11-05 23:21:17 +0530

    min Changes

---
+++ src/Questions/minChanges.java
@@ -0,0 +1,13 @@
+package Questions;
+
+public class minChanges {
+    public int minChanges(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length() - 1; i += 2) {
+            if (s.charAt(i) != s.charAt(i + 1)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
