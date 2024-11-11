522f163fb4c6c1e90b50afc97a0f7c69ac5369f8
Lav Kushwaha <lavkumar1224@gmail.com>
2024-11-11 12:37:50 +0530

    Rotten Oranges

--- src/_30_Graph_Theory/Questions/BFS/RottingOranges.java
+++ src/_30_Graph_Theory/Questions/BFS/RottingOranges.java
@@ -5,57 +5,65 @@ import java.util.Queue;
 //https://leetcode.com/problems/rotting-oranges/
 //994. Rotting Oranges
 
-class RottingOranges{
+class RottingOranges {
     public static int orangesRotting(int[][] grid) {
-       if(grid == null || grid.length == 0) return 0;
+        // If grid is empty, no rotting is possible
+        if (grid == null || grid.length == 0) return 0;
+
         int rows = grid.length;
         int cols = grid[0].length;
         Queue<int[]> queue = new LinkedList<>();
-        int count_fresh = 0;
-        //Put the position of all rotten oranges in queue
-        //count the number of fresh oranges
-        for(int i = 0 ; i < rows ; i++) {
-            for(int j = 0 ; j < cols ; j++) {
-                if(grid[i][j] == 2) {
-                    queue.offer(new int[]{i , j});
+        int totalOranges = 0, minutes = 0, rottenOranges = 0;
+
+        // Traverse the grid, adding rotten oranges to the queue and counting all oranges
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
                 }
-                if(grid[i][j] != 0) {
-                    count_fresh++;
+                if (grid[i][j] != 0) {
+                    totalOranges++;
                 }
             }
         }
-       
-        if(count_fresh == 0) return 0;
-        int countMin = 0, cnt = 0;
-        int dx[] = {0, 0, 1, -1};
-        int dy[] = {1, -1, 0, 0};
-        
-        //bfs starting from initially rotten oranges
-        while(!queue.isEmpty()) {
+
+        // If there are no oranges, return 0
+        if (totalOranges == 0) return 0;
+
+        // Directions array for moving up, down, left, and right
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        // BFS to spread the rot from rotten oranges to fresh oranges
+        while (!queue.isEmpty()) {
             int size = queue.size();
-            cnt += size; 
-            for(int i = 0 ; i < size ; i++) {
+            rottenOranges += size;
+
+            for (int i = 0; i < size; i++) {
                 int[] point = queue.poll();
-                for(int j = 0;j<4;j++) {
+
+                for (int j = 0; j < 4; j++) {
                     int x = point[0] + dx[j];
                     int y = point[1] + dy[j];
-                    
-                    if(x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 || 
-                    grid[x][y] == 2) continue;
-                    
+
+                    // Check bounds and if the neighboring orange is fresh
+                    if (x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] != 1) {
+                        continue;
+                    }
+
+                    // Mark the fresh orange as rotten and add it to the queue
                     grid[x][y] = 2;
-                    queue.offer(new int[]{x , y});
+                    queue.offer(new int[]{x, y});
                 }
             }
-            if(queue.size() != 0) {
-                countMin++;
+
+            // Increment minutes if there are still oranges to rot
+            if (!queue.isEmpty()) {
+                minutes++;
             }
         }
-        return count_fresh == cnt ? countMin : -1;
-    }
-    public static void main(String args[]) {
-        int arr[][]={ {2,1,1} , {1,1,0} , {0,1,1} };
-        int rotting = orangesRotting(arr);
-        System.out.println("Minimum Number of Minutes Required "+rotting);
+
+        // If all oranges have rotted, return minutes, otherwise return -1
+        return (totalOranges == rottenOranges) ? minutes : -1;
     }
 }
