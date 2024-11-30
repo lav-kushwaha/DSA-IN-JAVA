7a68f7d6d8e8fd56ff9e705468a547310ce94e89
Lav Kushwaha <lavkumar1224@gmail.com>
2024-11-30 20:21:33 +0530

    valid arrangement

---
+++ src/Questions/Questions/validArrangement.java
@@ -0,0 +1,56 @@
+package Questions.Questions;
+
+import java.util.*;
+
+//https://leetcode.com/problems/valid-arrangement-of-pairs/description/
+//2097. Valid Arrangement of Pairs
+
+class validArrangement {
+    public int[][] validArrangement(int[][] pairs) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        
+        Map<Integer, Integer> indegree = new HashMap<>();
+        Map<Integer, Integer> outdegree = new HashMap<>();
+
+        for (int[] edge : pairs) {
+            int u = edge[0];
+            int v = edge[1];
+            
+            adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+            outdegree.put(u, outdegree.getOrDefault(u, 0) + 1);
+            indegree.put(v, indegree.getOrDefault(v, 0) + 1);
+        }
+
+        int startNode = pairs[0][0];
+        for (int node : adj.keySet()) {
+            if (outdegree.getOrDefault(node, 0) - indegree.getOrDefault(node, 0) == 1) {
+                startNode = node;
+                break;
+            }
+        }
+
+        List<Integer> eulerPath = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(startNode);
+
+        while (!stack.isEmpty()) {
+            int curr = stack.peek();
+            if (adj.containsKey(curr) && !adj.get(curr).isEmpty()) {
+                int neighbor = adj.get(curr).remove(adj.get(curr).size() - 1);
+                stack.push(neighbor);
+            } else {
+                eulerPath.add(curr);
+                stack.pop();
+            }
+        }
+
+        Collections.reverse(eulerPath);
+        int[][] result = new int[eulerPath.size() - 1][2];
+        for (int i = 0; i < eulerPath.size() - 1; i++) {
+            result[i][0] = eulerPath.get(i);
+            result[i][1] = eulerPath.get(i + 1);
+        }
+
+        return result;
+    }
+}
