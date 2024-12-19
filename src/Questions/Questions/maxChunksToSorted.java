c914e6dabbe2c63a6fc897ca13aacb9a4de9881f
Lav Kushwaha <89603685+lav-kushwaha@users.noreply.github.com>
2024-12-20 00:20:38 +0530

    Update maxChunksToSorted.java

--- src/Questions/Questions/maxChunksToSorted.java
+++ src/Questions/Questions/maxChunksToSorted.java
@@ -6,14 +6,12 @@ class maxChunksToSorted {
     public int maxChunksToSorted(int[] arr) {
         if (arr == null || arr.length == 0)
             return 0;
-
         int count = 0, max = 0;
         for (int i = 0; i < arr.length; i++) {
             max = Math.max(max, arr[i]);
             if (max == i)
                 count++;
         }
-
         return count;
     }
 }
