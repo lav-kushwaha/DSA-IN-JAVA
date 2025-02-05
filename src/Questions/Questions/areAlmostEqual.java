0f98728e6b7fd32f6dfc085014ddb66f92fad2a2
Lav Kushwaha <lavkumar1224@gmail.com>
2025-02-05 23:25:39 +0530

    1790. Check if One String Swap Can Make Strings Equal

---
+++ src/Questions/Questions/areAlmostEqual.java
@@ -0,0 +1,26 @@
+package Questions.Questions;
+
+//https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/description/
+//1790. Check if One String Swap Can Make Strings Equal
+class areAlmostEqual {
+    public boolean areAlmostEqual(String s1, String s2) {
+        int i=-1, j=-1;
+        int cnt=0;
+
+        for(int k=0; k<s1.length(); k++){
+            if(s1.charAt(k)!=s2.charAt(k)){
+                cnt++;
+                if(i==-1) i=k;
+                else if(j==-1) j=k;
+            }
+        }
+
+        if(cnt==0) return true;
+        else if(cnt==2 && s1.charAt(i)==s2.charAt(j) && s1.charAt(j)==s2.charAt(i)){
+            return true;
+        }
+
+
+        return false;
+    }
+}
