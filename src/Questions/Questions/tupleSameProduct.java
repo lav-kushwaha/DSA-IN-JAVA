e3ce12e9debcd7df3a459222908bf48a2564b34c
Lav Kushwaha <lavkumar1224@gmail.com>
2025-02-06 22:29:32 +0530

    tupleSameProduct

---
+++ src/Questions/Questions/tupleSameProduct.java
@@ -0,0 +1,21 @@
+package Questions.Questions;
+
+import java.util.HashMap;
+
+//https://leetcode.com/problems/tuple-with-same-product/description/
+//1726. Tuple with Same Product
+class tupleSameProduct {
+    public int tupleSameProduct(int[] nums) {
+        HashMap<Integer, Integer> productCount = new HashMap<>();
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++)
+            for (int j = i+1; j < nums.length; j++) {
+                int product = nums[i] * nums[j];
+                result += 8 * productCount.getOrDefault(product, 0);
+                productCount.put(product, productCount.getOrDefault(product, 0) + 1);
+            }
+
+        return result;
+    }
+}
