858587a5b4c4d3567a6c78db5d6496421cdad75d
Lav Kushwaha <lavkumar1224@gmail.com>
2025-02-27 23:21:27 +0530

    lenLongestFibSubseq

---
+++ src/Questions/Questions/lenLongestFibSubseq.java
@@ -0,0 +1,34 @@
+package Questions.Questions;
+
+import java.util.*;
+
+//https://leetcode.com/problems/length-of-longest-fibonacci-subsequence/description/
+//873. Length of Longest Fibonacci Subsequence
+class lenLongestFibSubseq {
+
+    public int lenLongestFibSubseq(int[] arr) {
+        int n = arr.length;
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : arr) {
+            numSet.add(num);
+        }
+
+        int maxLen = 0;
+        for (int start = 0; start < n; ++start) {
+            for (int next = start + 1; next < n; ++next) {
+                int prev = arr[next];
+                int curr = arr[start] + arr[next];
+                int len = 2;
+
+                while (numSet.contains(curr)) {
+                    int temp = curr;
+                    curr += prev;
+                    prev = temp;
+                    maxLen = Math.max(maxLen, ++len);
+                }
+            }
+        }
+
+        return maxLen;
+    }
+}
