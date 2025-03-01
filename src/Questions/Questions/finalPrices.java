d314646b44bc3bcd8dd52fc46b6dd526c05e1521
Lav Kushwaha <lavkumar1224@gmail.com>
2025-03-01 22:42:57 +0530

    finalPrices

--- src/Questions/Questions/finalPrices.java
+++ src/Questions/Questions/finalPrices.java
@@ -2,11 +2,13 @@ package Questions.Questions;
 
 //https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/description/
 //1475. Final Prices With a Special Discount in a Shop
+
 class finalPrices {
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
         int[] result = prices.clone(); 
 
+        //loops
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (prices[j] <= prices[i]) {
