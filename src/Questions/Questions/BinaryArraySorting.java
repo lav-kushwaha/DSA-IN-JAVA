f8e838e104dcf821ffe6318fd21b43878967fe57
Lav Kushwaha <lavkumar1224@gmail.com>
2025-03-15 22:19:35 +0530

    BinaryArraySorting

--- src/Questions/Questions/BinaryArraySorting.java
+++ src/Questions/Questions/BinaryArraySorting.java
@@ -24,8 +24,26 @@ public class BinaryArraySorting {
         }
 
     }
+//    public static void binSort(int A[], int N) {
+//        int count = 0; // Count of 0's
+//
+//        for (int i = 0; i < N; i++) {
+//            if (A[i] == 0) {
+//                count++;
+//            }
+//        }
+//
+//        for (int i = 0; i < count; i++) {
+//            A[i] = 0;
+//        }
+//
+//        for (int i = count; i < N; i++) {
+//            A[i] = 1;
+//        }
+//
+//    }
     public static void main(String[] args) {
-        int [] arr = {1,1,0,1,1,0,0,1,1};
+        int [] arr = {1,0,1,1,0,0,1,0};
         int N = 5;
 
         binSort(arr,N);
