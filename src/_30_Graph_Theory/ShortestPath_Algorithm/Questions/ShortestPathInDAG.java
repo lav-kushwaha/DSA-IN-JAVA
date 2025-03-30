0fd2e60d91e3bf0637eebbb73b32495dcb7a7fa7
Lav Kushwaha <lavkumar1224@gmail.com>
2025-03-30 17:10:21 +0530

    Shortest Path in Undirected

--- src/_30_Graph_Theory/ShortestPath_Algorithm/Questions/ShortestPathInDAG.java
+++ src/_30_Graph_Theory/ShortestPath_Algorithm/Questions/ShortestPathInDAG.java
@@ -1,22 +1,18 @@
 package _30_Graph_Theory.ShortestPath_Algorithm.Questions;
 
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
 //https://www.geeksforgeeks.org/problems/shortest-path-in-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=shortest-path-in-undirected-graph
-//Shortest path in Directed Acyclic Graph
+//Shortest Path in a Directed Acyclic Graph (DAG) using Topological Sorting + Relaxation
 class ShortestPathInDAG {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int n = 6, m = 7;
         int[][] edge = {{0,1,2},{0,4,1},{4,5,4},{4,2,2},{1,2,3},{2,3,6},{5,3,1}};
         ShortestPathInDAG obj = new ShortestPathInDAG();
-        int res[] = obj.shortestPath(n, m, edge);
-//        for (int i = 0; i < n; i++) {
-//            System.out.print(res[i] + " ");
-//        }
-//        System.out.println();
+        int res[] = obj.shortestPath(n, m, edge, 0);  // Source = 0 start from node 0.
+
+        System.out.println("Shortest distances: " + Arrays.toString(res));
     }
 
     static class Pair {
@@ -25,31 +21,24 @@ class ShortestPathInDAG {
             this.first = _first;
             this.second = _second;
         }
-
-        //To print adj list we have override toString.
-//        @Override
-//        public String toString() {
-//            return "(" + first + ", " + second + ")";
-//        }
     }
 
-    private void topoSort(int node, ArrayList<ArrayList<Pair>> adj, int vis[], Stack<Integer> st) {
-        vis[node] = 1;
+    private void topoSort(int node, ArrayList<ArrayList<Pair>> adj, boolean vis[], Stack<Integer> st) {
+        vis[node] = true;
         for (Pair it : adj.get(node)) {
             int v = it.first;
-            if (vis[v] == 0) {
+            if (!vis[v]) {
                 topoSort(v, adj, vis, st);
             }
         }
         st.push(node);
     }
 
-    public int[] shortestPath(int N, int M, int[][] edges) {
+    public int[] shortestPath(int N, int M, int[][] edges, int src) {
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             adj.add(new ArrayList<>());
         }
-
 
         // Building adjacency list
         for (int i = 0; i < M; i++) {
@@ -59,32 +48,31 @@ class ShortestPathInDAG {
             adj.get(u).add(new Pair(v, wt));
         }
 
-        //printing adj list.
-//        for (int i = 0; i < adj.size(); i++) {
-//            System.out.print("List " + i + ": ");
-//            for (Pair p : adj.get(i)) {
-//                System.out.print(p + " ");
-//            }
-//            System.out.println();
-//        }
-
-
-        int vis[] = new int[N];
+        // Topological Sort using DFS
+        boolean vis[] = new boolean[N];
         Stack<Integer> st = new Stack<>();
         for (int i = 0; i < N; i++) {
-            if (vis[i] == 0) {
+            if (!vis[i]) {
                 topoSort(i, adj, vis, st);
             }
         }
-        System.out.println(st);
 
+        // Print Topological Order (For Debugging)
+        System.out.print("Topological Order: ");
+        for (Integer node : st) {
+            System.out.print(node + " ");
+        }
+        System.out.println();
+
+        // Initialize distances
         int dist[] = new int[N];
-        Arrays.fill(dist, (int)(1e9));
-        dist[0] = 0;
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0; //src node dist initially 0
 
+        // Relax edges in Topological Order
         while (!st.isEmpty()) {
             int node = st.pop();
-            if (dist[node] != (int)(1e9)) {
+            if (dist[node] != Integer.MAX_VALUE) {
                 for (Pair it : adj.get(node)) {
                     int v = it.first;
                     int wt = it.second;
@@ -95,9 +83,11 @@ class ShortestPathInDAG {
             }
         }
 
+        // Convert unreachable distances to -1
         for (int i = 0; i < N; i++) {
-            if (dist[i] == (int)(1e9)) dist[i] = -1;
+            if (dist[i] == Integer.MAX_VALUE) dist[i] = -1;
         }
+
         return dist;
     }
 }
---
+++ src/_30_Graph_Theory/ShortestPath_Algorithm/Questions/ShortestPathInUndirected.java
@@ -0,0 +1,59 @@
+package _30_Graph_Theory.ShortestPath_Algorithm.Questions;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+//https://www.geeksforgeeks.org/problems/shortest-path-in-undirected-graph-having-unit-distance/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=shortest-path-in-undirected-graph-having-unit-distance
+//Shortest Path in Undirected
+class ShortestPathInUndirected {
+    public int[] shortestPath(int[][] edges, int n, int m, int src) {
+        // Create an adjacency list for storing the undirected graph.
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+
+        // Distance array initialized with Integer.MAX_VALUE
+        int dist[] = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        // BFS Implementation
+        Queue<Integer> q = new LinkedList<>();
+        q.add(src);
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (int it : adj.get(node)) {
+                if (dist[node] + 1 < dist[it]) {
+                    dist[it] = 1 + dist[node];
+                    q.add(it);
+                }
+            }
+        }
+
+        // Unreachable nodes are marked as -1
+        for (int i = 0; i < n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) {
+                dist[i] = -1;
+            }
+        }
+        return dist;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int n = 9, m = 10;
+        int[][] edge = {{0,1},{0,3},{3,4},{4,5},{5,6},{1,2},{2,6},{6,7},{7,8},{6,8}};
+
+        ShortestPathInUndirected obj = new ShortestPathInUndirected();
+        int res[] = obj.shortestPath(edge, n, m, 0);
+        for (int i = 0; i < n; i++) {
+            System.out.print(res[i] + " ");
+        }
+        System.out.println();
+    }
+}
