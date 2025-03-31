144b3a620facb5332228928617d3744ab07352cc
Lav Kushwaha <lavkumar1224@gmail.com>
2025-03-31 23:04:51 +0530

    Dijkstra Algorithm Using PQ and TreeSet

--- src/_30_Graph_Theory/ShortestPath_Algorithm/Dijkstra_Algorithm/DijkstraAlgoUsingPQ/DijkstraAlgorithm.java
+++ src/_30_Graph_Theory/ShortestPath_Algorithm/Dijkstra_Algorithm/DijkstraAlgoUsingPQ/DijkstraAlgorithm.java
@@ -1,20 +1,20 @@
-package _30_Graph_Theory.ShortestPath_Algorithm.DijkstraAlgoUsingPQ;
+package _30_Graph_Theory.ShortestPath_Algorithm.Dijkstra_Algorithm.DijkstraAlgoUsingPQ;
 
 import java.util.*;
 
 //https://www.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1
 //Dijkstra Algorithm to finding the shortest path.
-class iPair {
+class Pair {
     int first, second;
 
-    public iPair(int first, int second) {
+    public Pair(int first, int second) {
         this.first = first;
         this.second = second;
     }
 }
 
 public class DijkstraAlgorithm {
-    public static ArrayList<Integer> dijkstra(ArrayList<ArrayList<iPair>> adj, int src) {
+    public static ArrayList<Integer> dijkstra(ArrayList<ArrayList<Pair>> adj, int src) {
         int n = adj.size();
         ArrayList<Integer> dis = new ArrayList<>();
 
@@ -24,26 +24,26 @@ public class DijkstraAlgorithm {
         }
         
         // Min-Heap to prioritize smaller distances
-        PriorityQueue<iPair> pq = new PriorityQueue<>((a, b) -> a.second - b.second);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.second - b.second);
         
         // Initialize source distance as 0
         dis.set(src, 0);
-        pq.add(new iPair(src, 0));
+        pq.add(new Pair(src, 0));
 
         while (!pq.isEmpty()) {
-            iPair current = pq.poll();
+            Pair current = pq.poll();
             int node = current.first;
 //            int dist = current.second;
 
             // Iterate over adjacent nodes
-            for (iPair neighbor : adj.get(node)) {
+            for (Pair neighbor : adj.get(node)) {
                 int v = neighbor.first;
                 int wt = neighbor.second;
 
                 // Relaxation step
                 if (dis.get(node) + wt < dis.get(v)) {
                     dis.set(v, dis.get(node) + wt);
-                    pq.add(new iPair(v, dis.get(v))); // Push updated distance into min-heap
+                    pq.add(new Pair(v, dis.get(v))); // Push updated distance into min-heap
                 }
             }
         }
@@ -53,19 +53,19 @@ public class DijkstraAlgorithm {
 
     public static void main(String[] args) {
         int n = 5; // Number of nodes
-        ArrayList<ArrayList<iPair>> adj = new ArrayList<>();
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
 
         // Adding edges (Graph Representation)
-        adj.get(0).add(new iPair(1, 2));
-        adj.get(0).add(new iPair(2, 4));
-        adj.get(1).add(new iPair(2, 1));
-        adj.get(1).add(new iPair(3, 7));
-        adj.get(2).add(new iPair(4, 3));
-        adj.get(3).add(new iPair(4, 1));
+        adj.get(0).add(new Pair(1, 2));
+        adj.get(0).add(new Pair(2, 4));
+        adj.get(1).add(new Pair(2, 1));
+        adj.get(1).add(new Pair(3, 7));
+        adj.get(2).add(new Pair(4, 3));
+        adj.get(3).add(new Pair(4, 1));
 
         // Running Dijkstra from node 0
         ArrayList<Integer> shortestDistances = dijkstra(adj, 0);
---
+++ src/_30_Graph_Theory/ShortestPath_Algorithm/Dijkstra_Algorithm/DijkstraAlgoUsingTreeSet/DijkstraAlgorithm.java
@@ -0,0 +1,67 @@
+package _30_Graph_Theory.ShortestPath_Algorithm.Dijkstra_Algorithm.DijkstraAlgoUsingTreeSet;
+
+import java.util.*;
+
+class Pair implements Comparable<Pair> {
+    int vertex, weight;
+
+    public Pair(int vertex, int weight) {
+        this.vertex = vertex;
+        this.weight = weight;
+    }
+
+    public int compareTo(Pair other) {
+        return Integer.compare(this.weight, other.weight);
+    }
+}
+
+public class DijkstraAlgorithm {
+    public static ArrayList<Integer> dijkstra(ArrayList<ArrayList<Pair>> adj, int src) {
+        int n = adj.size();
+        ArrayList<Integer> dis = new ArrayList<>(Collections.nCopies(n, Integer.MAX_VALUE));
+        
+        // TreeSet to efficiently extract the minimum distance node
+        TreeSet<Pair> set = new TreeSet<>();
+        dis.set(src, 0);
+        set.add(new Pair(src, 0));
+
+        while (!set.isEmpty()) {
+            Pair current = set.pollFirst(); // Extract the node with minimum distance
+            int node = current.vertex;
+
+            for (Pair neighbor : adj.get(node)) {
+                int v = neighbor.vertex;
+                int wt = neighbor.weight;
+
+                // Relaxation step
+                if (dis.get(node) + wt < dis.get(v)) {
+                    set.remove(new Pair(v, dis.get(v))); // Remove old distance if present
+                    dis.set(v, dis.get(node) + wt);
+                    set.add(new Pair(v, dis.get(v))); // Insert updated distance
+                }
+            }
+        }
+        return dis;
+    }
+
+    public static void main(String[] args) {
+        int n = 5; // Number of nodes
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Adding edges (Graph Representation)
+        adj.get(0).add(new Pair(1, 2));
+        adj.get(0).add(new Pair(2, 4));
+        adj.get(1).add(new Pair(2, 1));
+        adj.get(1).add(new Pair(3, 7));
+        adj.get(2).add(new Pair(4, 3));
+        adj.get(3).add(new Pair(4, 1));
+
+        // Running Dijkstra from node 0
+        ArrayList<Integer> shortestDistances = dijkstra(adj, 0);
+        System.out.println("Shortest distances from node 0: " + shortestDistances);
+    }
+}
