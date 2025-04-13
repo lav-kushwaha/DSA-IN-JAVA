dc02936d22805c7251652585271b17419c0a9dd6
Lav Kushwaha <lavkumar1224@gmail.com>
2025-04-14 00:02:34 +0530

    Minimum Multiplications to reach End Using BFS

---
+++ src/_30_Graph_Theory/ShortestPath_Algorithm/Questions/MinimumMultiplicationsToReachEnd.java
@@ -0,0 +1,47 @@
+package _30_Graph_Theory.ShortestPath_Algorithm.Questions;
+
+import java.util.*;
+
+//https://www.geeksforgeeks.org/problems/minimum-multiplications-to-reach-end/1
+//Minimum Multiplications to reach End
+class MinimumMultiplicationsToReachEnd {
+    
+    class Pair{
+        int num, steps;
+        Pair(int num, int steps){
+            this.num = num;
+            this.steps = steps;
+        }
+    }
+    
+    int minimumMultiplications(int[] arr, int start, int end) {
+    
+    if (start == end) return 0; 
+        
+    int mod = 100000;
+    int[] dist = new int[mod];
+    Arrays.fill(dist, Integer.MAX_VALUE); 
+    dist[start] = 0;
+    
+    Queue<Pair> queue = new LinkedList<>();
+    queue.add(new Pair(start, 0));
+    
+    while (!queue.isEmpty()) {
+        Pair curr = queue.poll();
+        int currNum = curr.num;
+        int currSteps = curr.steps;
+        
+        for (int i = 0; i < arr.length; i++) {
+            int num = (arr[i] * currNum) % mod;
+            if (currSteps + 1 < dist[num]) {
+                dist[num] = currSteps + 1;
+                if (num == end) return currSteps + 1;
+                queue.add(new Pair(num, currSteps + 1));
+            }
+        }
+    }
+    
+    return -1;
+}
+
+}
