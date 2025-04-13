824ab3ec0d28b4827ab7780d25d6d19e52f91ad3
Lav Kushwaha <lavkumar1224@gmail.com>
2025-04-13 22:25:42 +0530

    Peak Element

--- src/Questions/Questions/PeakElement.java
+++ src/Questions/Questions/PeakElement.java
@@ -1,49 +1,20 @@
 package Questions.Questions;
 
-//https://www.geeksforgeeks.org/problems/peak-element/0
-//Peak element
+class peakElement {
 
-class PeakElement {
-        public static int peakElement(int[] arr, int n) {
-
-        int start = 0;
-        int end = n - 1;
-
-        while (start < end) {
-
-            int mid = start + (end - start) / 2; //To find mid
-
-            // Compare middle element with its end neighbor
-            if (arr[mid] < arr[mid + 1]) {
-                start = mid + 1; // Peak must be on the end side
-            } else {
-                end = mid; // Peak must be on the start side (including mid)
-            }
+    public int peakElement(int[] arr) {
+        int n = arr.length;
+        if (n == 1) return 0;
+        if (arr[0] > arr[1]) return 0;
+        if (arr[n - 1] > arr[n - 2]) return n - 1;
+        int lo = 1, hi = n - 2;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) return mid;
+            if (arr[mid] < arr[mid + 1]) lo = mid + 1;
+            else
+                hi = mid - 1;
         }
-
-        // At the end, start and end will converge to the peak element index
-        return start;
-    }
-
-    public static void main(String[] args) {
-        // Test case 1
-        int[] arr1 = {1, 2, 3};
-        int peakIndex1 = peakElement(arr1, arr1.length);
-        System.out.println("Peak index for arr1: " + peakIndex1 + " (Expected: 2)");
-
-        // Test case 2
-        int[] arr2 = {1, 1, 1, 2, 1, 1, 1};
-        int peakIndex2 = peakElement(arr2, arr2.length);
-        System.out.println("Peak index for arr2: " + peakIndex2 + " (Expected: any of 0, 1, 3, 5, 6)");
-
-        // Test case 3
-        int[] arr3 = {10, 20, 15, 2, 23, 90, 67};
-        int peakIndex3 = peakElement(arr3, arr3.length);
-        System.out.println("Peak index for arr3: " + peakIndex3 + " (Expected: any of 1, 5)");
+        return 0;
     }
 }
-
-/*
-A peak element in an array is an element that is greater than or equal to its adjacent elements;
-for example, in the array [1, 3, 2, 4, 1], the elements 3 and 4 are peaks.
-*/
