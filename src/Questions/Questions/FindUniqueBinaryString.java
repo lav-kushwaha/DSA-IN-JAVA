68fa79d842de431967ba351ca17a02849bdd89c0
Lav Kushwaha <lavkumar1224@gmail.com>
2025-04-26 23:46:37 +0530

    find unique binary string

---
+++ src/Questions/Questions/FindUniqueBinaryString.java
@@ -0,0 +1,15 @@
+package Questions.Questions;
+
+class FindUniqueBinaryString {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i].charAt(i) == '0') {
+                result.append('1');
+            } else {
+                result.append('0');
+            }
+        }
+        return result.toString();
+    }
+}
