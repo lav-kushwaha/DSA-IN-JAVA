0294b9d13f10340275795a83fee53c1fed64be47
Lav Kushwaha <lavkumar1224@gmail.com>
2025-05-02 23:18:44 +0530

    findUnion

--- src/Questions/Questions/findUnion.java
+++ src/Questions/Questions/findUnion.java
@@ -1,46 +1,18 @@
 package Questions.Questions;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
-class findUnion
-{
-    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
-    {
-         ArrayList<Integer> ans = new ArrayList<>();
-        
-        int i = 0, j = 0;
-        
-        while(i < n && j < m){
-            if(arr1[i] < arr2[j]){
-                if(i == 0 || arr1[i] != arr1[i - 1])
-                    ans.add(arr1[i]);
-                i++;
-            }
-            else if(arr1[i] > arr2[j]){
-                if(j == 0 || arr2[j] != arr2[j - 1])
-                    ans.add(arr2[j]);
-                j++;
-            }
-            else{
-                if((i == 0 || arr1[i] != arr1[i - 1]) && (j == 0 || arr2[j] != arr2[j - 1]))
-                    ans.add(arr1[i]);
-                i++;
-                j++;
-            }
+class findUnion {
+    public static int findUnion(int a[], int b[]) {
+        // code here
+        //Your code here
+         HashSet<Integer> s = new HashSet<Integer>();
+        for(int i=0;i<a.length;i++) { 
+            s.add(a[i]);
         }
-        
-        while(i < n){
-            if(i == 0 || arr1[i] != arr1[i - 1])
-                ans.add(arr1[i]);
-            i++;
+        for(int i=0;i<b.length;i++) {
+            s.add(b[i]);
         }
-        
-        while(j < m){
-            if(j == 0 || arr2[j] != arr2[j - 1])
-                ans.add(arr2[j]);
-            j++;
-        }
-        
-        return ans;
+        return s.size();
     }
 }
