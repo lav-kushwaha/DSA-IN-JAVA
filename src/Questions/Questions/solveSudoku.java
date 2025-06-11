24664f4d74d1302165ccd4569d90c9dac3aee759
Lav Kushwaha <lavkumar1224@gmail.com>
2025-06-11 21:39:22 +0530

    solveSudoku

---
+++ src/Questions/Questions/solveSudoku.java
@@ -0,0 +1,65 @@
+package Questions.Questions;
+
+class solveSudoku {
+      public static boolean solveSudoku(char[][] board) {
+        int[] emptyCell = findEmptyCell(board);
+        if (emptyCell == null) {
+            return true;
+        }
+
+        int row = emptyCell[0];
+        int col = emptyCell[1];
+
+        for (char num = '1'; num <= '9'; num++) {
+            if (isValid(board, row, col, num)) {
+                board[row][col] = num;
+
+                if (solveSudoku(board)) {
+                    return true;
+                }
+
+                board[row][col] = '.';
+            }
+        }
+
+        return false;
+    }
+
+    public static int[] findEmptyCell(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean isValid(char[][] board, int row, int col, char num) {
+        // Check if the number is already present in the row
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i + startRow][j + startCol] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
