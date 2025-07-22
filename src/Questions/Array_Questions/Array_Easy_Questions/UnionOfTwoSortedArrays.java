a459e677de1f54b2aa17b25f6c04e8a338d6e606
Lav Kushwaha <lavkumar1224@gmail.com>
2025-07-22 06:09:45 +0530

    Union of two sorted arrays

--- src/Questions/Array_Questions/Array_Easy_Questions/UnionOfTwoSortedArrays.java
+++ src/Questions/Array_Questions/Array_Easy_Questions/UnionOfTwoSortedArrays.java
@@ -1,2 +1,54 @@
-package Questions.Array_Questions.Array_Easy_Questions;public class UnionOfTwoSortedArrays {
+package Questions.Array_Questions.Array_Easy_Questions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnionOfTwoSortedArrays {
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+
+        int i = 0;
+        int j = 0;
+
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                addIfNotDuplicates(ans, a[i]);
+                i++;
+            } else if (a[i] > b[j]) {
+                addIfNotDuplicates(ans, b[j]);
+                j++;
+            } else {
+                addIfNotDuplicates(ans, a[i]);
+                i++;
+                j++;
+            }
+        }
+
+        while (i < a.length) {
+            addIfNotDuplicates(ans, a[i]);
+            i++;
+        }
+
+        while (j < b.length) {
+            addIfNotDuplicates(ans, b[j]);
+            j++;
+        }
+
+        return ans;
+    }
+
+    public static void addIfNotDuplicates(List<Integer> list, int value) {
+        if (list.isEmpty() || list.get(list.size() - 1) != value) {
+            list.add(value);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
+
+//Intuition.
+//1.Use two pointers to walk through both sorted arrays
+//2.Compare and add the smaller (or equal) element
+//3.Skip duplicates by checking the last inserted item
